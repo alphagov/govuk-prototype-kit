6002e498a16ecdc967aaf935f924bba2b5b5bdf5
joelanman <joelanman@gmail.com>
2014-01-29 17:00:24 +0000

    moving from sass to node sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,19 @@ module.exports = function(grunt){
     // Removes the contents of the public directory
     clean: ["public/"],
     // Builds Sass
+
     sass: {
       dev: {
         files: {
           'public/stylesheets/application.css': 'app/assets/stylesheets/application.scss'
         },
         options: {
-          loadPath: ['node_modules/govuk_frontend_toolkit/govuk_frontend_toolkit/stylesheets'],
-          style: 'expanded'
+          includePaths: ['node_modules/govuk_frontend_toolkit/govuk_frontend_toolkit/stylesheets'],
+          outputStyle: 'expanded'
         } 
       }
     },
+
     // Copies templates and assets from external modules and dirs
     copy: {
       govuk_template: {
@@ -48,12 +50,13 @@ module.exports = function(grunt){
     'grunt-contrib-clean',
     'grunt-contrib-copy',
     'grunt-contrib-watch',
-    'grunt-contrib-sass'
+    'grunt-sass'
   ].forEach(function (task) {
     grunt.loadNpmTasks(task);
   });
 
   grunt.registerTask('default', [
+    'sass',
     'copy:govuk_template',
     'copy:govuk_assets',
     'watch'
