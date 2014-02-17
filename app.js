2d3f80552f50bcf068c58b71ef96f21145ab89b5
tombye <tombaromba@gmail.com>
2014-02-17 20:50:27 +0000

    Add Hogan-middleware
    Contains following changes to NPM version:
    
    * all file watching removed
    * compiles templates from additional views folder

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 var express = require('express'),
-    cons = require('consolidate'),
     app = express(),
-    mustacheRender = require("./lib/mustacheRender").mustacheRender,
     port = (process.env.PORT || 3000);
 
 // Application settings
-app.engine('html', cons.mustache);
+app.engine('html', require(__dirname + '/lib/template-engine.js').__express);
 app.set('view engine', 'html');
+app.set('vendorViews', __dirname + '/govuk/views');
 app.set('views', __dirname + '/views');
 
 // Middleware to serve static assets
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk/public'));
-
-// middleware to wrap mustache views in govuk template
-
-app.use(mustacheRender);
-
-//
 
 var commonHead = '<link href="/public/stylesheets/application.css" rel="stylesheet" type="text/css" />';
 
