c479e6f68b26162da883fc3d49b036de80656386
tombye <tombaromba@gmail.com>
2014-02-17 21:15:23 +0000

    Add Grunt task to convert template
    Converts to proposed version of mustache that supports inheritance.

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,10 +92,22 @@ module.exports = function(grunt){
   ].forEach(function (task) {
     grunt.loadNpmTasks(task);
   });
+  
+  grunt.registerTask(
+    'convert_template',
+    'Converts the govuk_template to use mustache inheritance',
+    function () {
+      var script = require(__dirname + '/lib/template-conversion.js');
+
+      script.convert();
+      grunt.log.writeln('govuk_template converted');
+    }
+  );
 
   grunt.registerTask('default', [
     'copy:govuk_template',
     'copy:govuk_assets',
+    'convert_template',
     'copy:govuk_frontend_toolkit',
     'replace',
     'sass',
