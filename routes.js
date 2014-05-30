5a2e113bd90a043de8f733c25de7ad92c6d13ff7
joelanman <joelanman@gmail.com>
2014-05-30 15:34:18 +0100

    added auto route feature

--- routes.js
+++ routes.js
@@ -7,45 +7,46 @@ module.exports = {
       
     });
 
-    app.get('/sample', function (req, res) {
-      
-      res.render('sample',
-                {'assetPath' : assetPath});
-    });
 
     /* Example pages */
 
     app.get('/examples/hello-world', function (req, res) {
-      res.render('examples/hello_world', {'message' : 'Hello world'});
+      res.render('examples/hello-world', {'message' : 'Hello world'});
     });
 
     app.get('/examples/inheritance', function (req, res) {
-      res.render('examples/inheritance/page_level', {'message' : 'Hello world'});
+      res.render('examples/inheritance/page-level', {'message' : 'Hello world'});
     });
 
     app.get('/examples/alpha', function (req, res) {
       res.render('examples/alpha/alpha', {'assetPath' : assetPath });    
     });
 
-    app.get('/examples/template-partial-areas', function (req, res) {
 
-      res.render('examples/template_partial_areas',
-                {'assetPath' : assetPath});
-      
+    app.get('/moop', function (req, res) {
+      res.render('examples/elements', {'assetPath' : assetPath });    
     });
 
-    /* Elements example pages */
+    app.get(/^\/(.+)/, function (req, res) {
 
-    app.get('/examples/elements/grid-layout', function (req, res) {
-      res.render('examples/elements/grid_layout', {'assetPath' : assetPath });    
-    });
+      var path = (req.params[0]);
 
-    app.get('/examples/elements/typography', function (req, res) {
-      res.render('examples/elements/typography', {'assetPath' : assetPath });    
-    });
-    
-    app.get('/examples/elements/forms', function (req, res) {
-      res.render('examples/elements/forms', {'assetPath' : assetPath });    
+      console.log('looking up ' + path);
+
+      res.render(path, {'assetPath' : assetPath }, function(err, html) {
+
+        if (err) {
+
+          res.send(404);
+
+        } else {
+
+          res.end(html);
+
+        }
+
+      });
+
     });
 
   }
