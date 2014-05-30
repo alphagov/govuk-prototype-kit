e49fca533fbc493eb04b68072dfb314ce6b24594
joelanman <joelanman@gmail.com>
2014-05-30 16:29:09 +0100

    restored template logging, tab formatting, spacing

--- app.js
+++ app.js
@@ -26,19 +26,12 @@ app.get(/^\/(.+)/, function (req, res) {
 	var path = (req.params[0]);
 
 	res.render(path, {'assetPath' : assetPath }, function(err, html) {
-
 		if (err) {
-
 			console.log(err);
-
 			res.send(404);
-
 		} else {
-
 			res.end(html);
-
 		}
-
 	});
 
 });
