9b526c7f741d421901f84278e5de3f087b4aff68
Gemma Leigh <gemma@little-digital.com>
2014-07-22 13:57:34 +0100

    Update govuk elements
    Buttons - Use ems rather than px, remove buttons styled as links
    Colours - Remove error colours (awating updated govuk front end toolkit
    npm)
    Details - Add details summary polyfill
    Helpers - Remove duplicate mixins ((awating updated govuk front end
    toolkit npm)
    Layout - Use .inner-block and .outer block mixins (awating updated
    govuk front end toolkit npm)
    Panels - Reduce border width on panels
    Tables - Remove background and dotted border on tables
    Typography - Set heading levels from xxlarge to xsmall
    Forms - Adjust spacing on chunky radio buttons to fit with new
    validation styles
    Forms - Date of birth - Add Modernizer for native date picker on touch
    devices
    
    Update all examples to use new heading classnames

---
+++ public/javascripts/application.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+  // Chunky checkboxes and radio buttons
+
+  if ($('.form').length>0) {
+
+    $(".block-label").each(function() {
+
+      // Add focus
+      $(".block-label input").focus(function() {
+        $("label[for='" + this.id + "']").addClass("add-focus");
+        }).blur(function() {
+        $("label").removeClass("add-focus");
+      });
+
+      // Add selected class
+      $('input:checked').parent().addClass('selected');
+
+    });
+
+    // Add/remove selected class
+    $('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
+
+      $('input:not(:checked)').parent().removeClass('selected');
+      $('input:checked').parent().addClass('selected');
+
+      $('.toggle-content').hide();
+
+      var target = $('input:checked').parent().attr('data-target');
+      $('#'+target).show();
+
+    });
+
+    // For pre-checked inputs, show toggled content
+    var target = $('input:checked').parent().attr('data-target');
+    $('#'+target).show();
+
+  }
+
+  // Aria support for details
+  // See /javascripts/vendor/details.polyfill.js
+
+});
