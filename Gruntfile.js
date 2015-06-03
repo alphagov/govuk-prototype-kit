21a6cb78340d608bb8ce8384d7af6679e26c7790
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2015-06-03 16:01:02 +0100

    Set grunt options to output Sass source maps
    Source maps allow the user to view and edit the Sass source files [in browser](https://developer.chrome.com/devtools/docs/css-preprocessors).
    
    * `style` is the same as `outputStyle` so was removed
    * `sourceComments` adds debug info to the output
    
    [These instructions](http://www.sitepoint.com/using-source-maps-debug-sass-chrome/) are easier to follow than the Google documentation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,13 @@ module.exports = function(grunt){
     sass: {
       dev: {
         options: {
-          style: "expanded",
-          sourcemap: true,
           includePaths: [
             'govuk_modules/govuk_template/assets/stylesheets',
             'govuk_modules/govuk_frontend_toolkit/stylesheets'
           ],
-          outputStyle: 'expanded'
+          outputStyle: 'expanded',
+          sourceComments: true,
+          sourceMap: true
         },
         files: [{
           expand: true,
