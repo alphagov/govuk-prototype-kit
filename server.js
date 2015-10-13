daba98ae142a1ca3307dd710bcb43225e1320c13
Joe Lanman <joelanman@gmail.com>
2015-10-13 15:11:36 +0100

    updated routes.js to use express v4 Router

--- server.js
+++ server.js
@@ -44,7 +44,13 @@ app.use(function (req, res, next) {
 
 // routes (found in app/routes.js)
 
-routes.bind(app);
+if (typeof(routes) != "function"){
+  console.log(routes.bind);
+  console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.")
+  routes.bind(app);
+} else {
+  app.use("/", routes);
+}
 
 // auto render any view that exists
 
