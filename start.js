5f85a5cd02cc41010257714f1349f9825693fa53
Tom Byers <tombaromba@gmail.com>
2015-10-13 20:22:21 +0100

    Remove unused code from start.js

--- start.js
+++ start.js
@@ -1,10 +1,4 @@
-var fs = require('fs'),
-    argv = require('minimist')(process.argv.slice(2)),
-    fileOptions = { encoding : 'utf-8' },
-    gruntfile;
-
-// start grunt
-gruntfile = __dirname + '/Gruntfile.js';
+var gruntfile = __dirname + '/Gruntfile.js';
 require(__dirname + '/node_modules/grunt/lib/grunt.js').cli({
   'gruntfile' : gruntfile
 });
