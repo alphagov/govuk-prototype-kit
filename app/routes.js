374faa7111b5d77c92b72d0d9a6076df6ff858b9
Alistair Duggin <alistair.duggin@btinternet.com>
2015-10-19 12:17:26 +0100

    Add second example and refactor for maintinability

--- app/routes.js
+++ app/routes.js
@@ -5,15 +5,4 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/link-looks-like-button', function (req, res) {
-  res.render('link-looks-like-button', { 'title' : 'Link looks like a button' });
-});
-
-
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name' : 'Foo' });
-});
-
-// add your routes here
-
 module.exports = router;
