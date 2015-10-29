c958cb85c23033b02e8f02cdbadd9db62b33a521
Rebecca Cottrell <r.e.cottrell@gmail.com>
2015-10-29 16:43:30 +0000

    Elements icons now display in the kit

--- server.js
+++ server.js
@@ -29,8 +29,10 @@ app.set('views', __dirname + '/app/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
+app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
+// Elements refers to icon folder instead of images folder
 
-app.use(express.favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico'))); 
+app.use(express.favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
 
 
 // send assetPath to all views
