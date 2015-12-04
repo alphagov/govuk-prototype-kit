8747302ea58b78b4e81f045497028da90ab56eb3
Edward Horsford <mail@edwardhorsford.com>
2015-12-04 17:52:36 +0000

    Add warning if  folder missing or module missing

--- start.js
+++ start.js
@@ -1,3 +1,15 @@
+// Check for `node_modules` folder and warn if missing
+var fs = require('fs');
+if (!fs.existsSync(__dirname + '/node_modules')) {
+  console.error('ERROR: Node module folder missing. Try running `npm install`');
+  process.exit(0);
+}
+
+// Only import utils if node_modules folder exists
+utils = require(__dirname + '/lib/utils.js');
+// Confirm that all modules in package.json exist
+utils.checkNodeModules();
+
 var gruntfile = __dirname + '/Gruntfile.js';
 require(__dirname + '/node_modules/grunt/lib/grunt.js').cli({
   'gruntfile' : gruntfile
@@ -7,3 +19,4 @@ process.on('SIGINT', function() {
   process.kill(process.pid, 'SIGTERM');
   process.exit();
 });
+
