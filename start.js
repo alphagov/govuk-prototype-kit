201501d2dde48a741d18bc31e4d9275db8d8e739
Edward Horsford <mail@edwardhorsford.com>
2016-01-11 11:18:31 +0000

    Only check for node_module folder, not individual packages

--- start.js
+++ start.js
@@ -5,11 +5,6 @@ if (!fs.existsSync(__dirname + '/node_modules')) {
   process.exit(0);
 }
 
-// Only import utils if node_modules folder exists
-utils = require(__dirname + '/lib/utils.js');
-// Confirm that all modules in package.json exist
-utils.checkNodeModules();
-
 var gruntfile = __dirname + '/Gruntfile.js';
 require(__dirname + '/node_modules/grunt/lib/grunt.js').cli({
   'gruntfile' : gruntfile
