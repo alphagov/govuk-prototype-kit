6ff3b6b3b640a196fc1f4c9cdbbdeee1b073530d
Gemma Leigh <hello@gemmaleigh.com>
2016-01-12 18:04:20 +0000

    Remove govuk template conversion
    Remove config files, conversion engine and grunt tasks used for
    template conversion.

--- Gruntfile.js
+++ Gruntfile.js
@@ -109,28 +109,14 @@ module.exports = function(grunt){
     'grunt-contrib-clean',
     'grunt-sass',
     'grunt-nodemon',
-    'grunt-text-replace',
     'grunt-concurrent'
   ].forEach(function (task) {
     grunt.loadNpmTasks(task);
   });
 
-  grunt.registerTask(
-    'convert_template',
-    'Converts the govuk_template to use mustache inheritance',
-    function () {
-      var script = require(__dirname + '/lib/template-conversion.js');
-
-      script.convert();
-      grunt.log.writeln('govuk_template converted');
-    }
-  );
-
   grunt.registerTask('generate-assets', [
     'clean',
     'copy',
-    'convert_template',
-    'replace',
     'sass'
   ]);
 
