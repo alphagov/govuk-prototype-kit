e481edfeb8b3ee3ca8578591ab0560618c5de392
Joe Lanman <joelanman@gmail.com>
2016-02-04 15:47:56 +0000

    add example pages for branching

--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
+  
   res.render('index');
+
 });
 
 
-// Example route: Passing data into a page
+// Example routes - feel free to delete these
+
+// Passing data into a page
+
 router.get('/examples/template-data', function (req, res) {
+
   res.render('examples/template-data', { 'name' : 'Foo' });
+
+});
+
+// Branching
+
+router.get('/examples/over18', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+	var over18 = req.query.over18;
+
+  if (over18 == "false"){
+
+    // redirect to the relevant page
+    res.redirect("/examples/under18");
+
+  } else {
+
+    // render the page requested
+    res.render('examples/over18');
+
+  }
+
 });
 
 // add your routes here
