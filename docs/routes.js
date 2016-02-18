6e0eb56f59bd20373fbc63d8a207715db5c24c4c
Edward Horsford <mail@edwardhorsford.com>
2016-02-18 15:59:54 +0000

    Minor updates

--- docs/routes.js
+++ docs/routes.js
@@ -6,23 +6,23 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/getting-started', function (req, res) {
+router.get('/install', function (req, res) {
   var url = "";
   var options = {
     uri: 'https://api.github.com/repos/alphagov/govuk_prototype_kit/releases/latest',
     headers: {'user-agent': 'node.js'}
-  }
+  };
   request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage. 
+      console.log(body); // Show the HTML for the Google homepage. 
       var data = JSON.parse(body);
       url = data.zipball_url;
       console.log("Release url is", url);
-      res.render('docs/getting-started', { 'releaseURL' : url });
+      res.render('docs/install', { 'releaseURL' : url });
     } else {
       url = "https://github.com/alphagov/govuk_prototype_kit/releases/latest";
       console.log("Error getting release URL: " + error);
-      res.render('docs/getting-started', { 'releaseURL' : url });
+      res.render('docs/install', { 'releaseURL' : url });
     }
 
   });
