4831fbda8d10464c6e209cb211729e25d489b6d4
Edward Horsford <mail@edwardhorsford.com>
2016-02-19 16:32:39 +0000

    Set up markdown rendering of .md files. And lots of other stuff. Soz

--- docs/routes.js
+++ docs/routes.js
@@ -1,34 +1,88 @@
 var express = require('express'),
+    fs      = require('fs'),
+    marked  = require('marked'),
+    path    = require('path'),
     request = require('request'),
-    router = express.Router();
+    router = express.Router(),
+    utils = require(__dirname + '/../lib/utils.js');
 
 router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/install', function (req, res) {
-  var url = "";
-  var options = {
-    uri: 'https://api.github.com/repos/alphagov/govuk_prototype_kit/releases/latest',
-    headers: {'user-agent': 'node.js'}
-  };
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body); // Show the HTML for the Google homepage. 
-      var data = JSON.parse(body);
-      url = data.zipball_url;
-      console.log("Release url is", url);
-      res.render('docs/install', { 'releaseURL' : url });
-    } else {
-      url = "https://github.com/alphagov/govuk_prototype_kit/releases/latest";
-      console.log("Error getting release URL: " + error);
-      res.render('docs/install', { 'releaseURL' : url });
-    }
+router.post('/examples', function (req, res) {
+  res.redirect('examples');
+});
 
-  });
+router.get('/install', function (req, res) {
+  url = utils.getLatestRelease();
+  res.render('install', { 'releaseURL' : url });
+});
+
+// Example routes - feel free to delete these
+
+// Passing data into a page
+
+router.get('/examples/template-data', function (req, res) {
+  console.log('rendering template data');
+
+  res.render('examples/template-data', { 'name' : 'Foo' });
 
 });
 
-// add your here
+// Branching
+
+router.get('/examples/over-18', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+  var over18 = req.query.over18;
+
+  if (over18 == "false"){
+
+    // redirect to the relevant page
+    res.redirect("examples/under-18");
+
+  } else {
+
+    // if over18 is any other value (or is missing) render the page requested
+    res.render('examples/over-18');
+
+  }
+
+});
+
+// Redirect documentation directory (just in case)
+router.get('/documentation/', function (req, res) {
+  res.redirect('documentation-and-examples');
+});
+
+// Pages in documentation folder are markdown
+router.get('/documentation/:page', function (req, res) {
+  redirectMarkdown(req.params.page, res);
+  var doc = fs.readFileSync(path.join(__dirname + '/documentation/' + req.params.page + ".md"), "utf8");
+  var html = marked(doc);
+  res.render("documentation_template", {"document": html});
+});
+
+// Pages in install folder are markdown
+router.get('/install/:page', function (req, res) {
+  redirectMarkdown(req.params.page, res);
+  var doc = fs.readFileSync(path.join(__dirname + '/documentation/install/' + req.params.page + ".md"), "utf8");
+  var html = marked(doc);
+  res.render("install_template", {"document": html});
+});
+
+// Strip off markdown extensions if present and redirect
+var redirectMarkdown = function (requestedPage, res){
+  if (requestedPage.slice(-3).toLowerCase() == ".md"){
+    res.redirect(requestedPage.slice(0, -3));
+  }
+  if (requestedPage.slice(-9).toLowerCase() == ".markdown"){
+    res.redirect(requestedPage.slice(0, -9));
+  }
+};
+
+
+// add routes here
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var path = require('path'),
     express = require('express'),
-    // nunjucks = require('express-nunjucks'),
     nunjucks = require('nunjucks'),
     routes = require(__dirname + '/app/routes.js'),
     docsRoutes = require(__dirname + '/docs/routes.js'),
@@ -17,7 +16,8 @@ var path = require('path'),
     username = process.env.USERNAME,
     password = process.env.PASSWORD,
     env      = process.env.NODE_ENV || 'development',
-    useAuth  = process.env.USE_AUTH || config.useAuth;
+    useAuth  = process.env.USE_AUTH || config.useAuth,
+    useDocs  = (config.useDocs == "true" ) ? true : false;
 
     env      = env.toLowerCase();
     useAuth  = useAuth.toLowerCase();
@@ -28,8 +28,8 @@ if (env === 'production' && useAuth === 'true'){
     app.use(utils.basicAuth(username, password));
 }
 
-var appViews = [__dirname + '/app/views', __dirname + '/lib/'];
-var docsViews = [__dirname + '/docs/views', __dirname + '/lib/'];
+var appViews = [__dirname + '/app/views/', __dirname + '/lib/'];
+var docsViews = [__dirname + '/docs/views/', __dirname + '/lib/'];
 
 nunjucks.configure(appViews, {
     autoescape: true,
@@ -55,6 +55,10 @@ app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/ass
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
 app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
 
+// TODO: is this the right way to serve these assets?
+docsApp.use('/assets/images', express.static(__dirname + '/docs/assets/images'));
+
+
 // Elements refers to icon folder instead of images folder
 app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
 
@@ -77,35 +81,41 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Create separate router for docs
-app.use("/docs", docsApp);
-// Docs under the /docs namespace
-docsApp.use("/", docsRoutes);
-
-
-// docs_app.set('views', [__dirname + '/lib/', __dirname + '/docs/views']);
-
 // routes (found in app/routes.js)
 if (typeof(routes) != "function"){
   console.log(routes.bind);
-  console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.")
+  console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.");
   routes.bind(app);
 } else {
   app.use("/", routes);
+}
+
+// Downloads returns a url to the zip of the latest release on github
+app.get('/download-latest', function (req, res) {
+  url = utils.getLatestRelease();
+  res.redirect(url);
+});
+
+if (useDocs){
+  // Create separate router for docs
+  app.use("/docs", docsApp);
+  // Docs under the /docs namespace
+  docsApp.use("/", docsRoutes);
 }
 
 // auto render any view that exists
 
 // App folder routes get priority
 app.get(/^\/([^.]+)$/, function (req, res) {
-  utils.matchRoutes('/', req, res);
+  utils.matchRoutes(req, res);
 });
 
 // Docs folder routes
-docsApp.get(/^\/([^.]+)$/, function (req, res) {
-  utils.matchRoutes('docs/', req, res);
-});
-
+if (useDocs){
+  docsApp.get(/^\/([^.]+)$/, function (req, res) {
+    utils.matchRoutes(req, res);
+  });
+}
 
 // start the app
 
