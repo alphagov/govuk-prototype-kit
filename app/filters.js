3016b63fb5bf28eac59f98f4c86fac46f262282c
Paul Smith <mail@airfront.co.uk>
2016-04-15 22:13:25 +0100

    corrects typo

--- app/filters.js
+++ app/filters.js
@@ -1,5 +1,5 @@
 /**
- * Instantiate object used store the methods registered as a
+ * Instantiate object used to store the methods registered as a
  * 'filter' (of the same name) within nunjucks. You can override
  * gov.uk core filters by creating filter methods of the same name.
  * @type {Object}
