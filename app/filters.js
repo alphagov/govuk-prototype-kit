093ca5652dbb4ed0c7a6d68953f93409dcecee1e
Paul Smith <mail@airfront.co.uk>
2016-05-18 11:06:10 +0100

    updates comment block

--- app/filters.js
+++ app/filters.js
@@ -35,10 +35,11 @@ module.exports = function(env) {
     {{ 'Paul' | sayHi }} => 'Hi Paul!'
 
     For more on filters and how to write them see the Nunjucks
+    documentation.
 
   ------------------------------------------------------------------ */
 
-  
+
 
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
