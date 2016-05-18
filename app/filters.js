cb2a1e67df2aa3aa752da1fd8654bf2cfa0a5e5a
Paul Smith <mail@airfront.co.uk>
2016-05-18 11:04:39 +0100

    updates filters comment with a bit of inline documentation

--- app/filters.js
+++ app/filters.js
@@ -9,12 +9,32 @@ module.exports = function(env) {
   var filters = {};
 
   /* ------------------------------------------------------------------
-    add your methods to the filter obj below this comment block:
-    @example
+    add your methods to the filters obj below this comment block:
+    @example:
 
     filters.sayHi = function(name) {
         return 'Hi ' + name + '!';
     }
+
+    Which in your templates would be used as:
+
+    {{ 'Paul' | sayHi }} => 'Hi Paul'
+
+    Notice the first argument of your filters method is whatever
+    gets 'piped' via '|' to the filter.
+
+    Filters can take additional arguments, for example:
+
+    filters.sayHi = function(name,tone) {
+      return (tone == 'formal' ? 'Greetings' : 'Hi') + ' ' + name + '!';
+    }
+
+    Which would be used like this:
+
+    {{ 'Paul' | sayHi('formal') }} => 'Greetings Paul!'
+    {{ 'Paul' | sayHi }} => 'Hi Paul!'
+
+    For more on filters and how to write them see the Nunjucks
 
   ------------------------------------------------------------------ */
 
