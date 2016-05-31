8118f88e7a2a7e72a4eaa598a9b55d949a16f108
Gemma Leigh <hello@gemmaleigh.com>
2016-05-31 14:49:04 +0100

    Add govuk-elements-sass to includePaths
    These files won’t need to be copied from govuk/modules.
    
    In theory govuk_modules isn’t needed either and these paths could be
    updated to /node_modules/govuk_repo_name but it provides a way to see
    what lives in each repo.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function(grunt){
           sourcemap: true,
           includePaths: [
             'govuk_modules/govuk_template/assets/stylesheets',
-            'govuk_modules/govuk_frontend_toolkit/stylesheets'
+            'govuk_modules/govuk_frontend_toolkit/stylesheets',
+            'govuk_modules/govuk-elements-sass/'
           ],
           outputStyle: 'expanded'
         },
