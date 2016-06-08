075c6c9817169f673bb4c3f96ba0c0828dcace99
Joe Lanman <joelanman@gmail.com>
2016-06-08 17:51:46 +0100

    auto store data

--- server.js
+++ server.js
@@ -41,8 +41,10 @@ nunjucks.setup({
   noCache: true
 }, app);
 
+
 // require core and custom filters, merges to one object
 // and then add the methods to nunjucks env obj
+
 nunjucks.ready(function(nj) {
   var coreFilters = require(__dirname + '/lib/core_filters.js')(nj),
     customFilters = require(__dirname + '/app/filters.js')(nj),
@@ -50,123 +52,165 @@ nunjucks.ready(function(nj) {
   Object.keys(filters).forEach(function(filterName) {
     nj.addFilter(filterName, filters[filterName]);
   });
-});
 
-// Middleware to serve static assets
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
-app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
-app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
+  app.use(function(req,res,next){
 
-// Elements refers to icon folder instead of images folder
-app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
+    // add nunjucks function to get values, needs to be here as they need access to req.session
 
-// Support for parsing data in POSTs
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+    nj.addGlobal("checked",function(name, value){
 
-// Support session data
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: Math.round(Math.random()*100000).toString()
-}));
+      if (req.session.data == undefined){
+        return "";
+      }
 
-// send assetPath to all views
-app.use(function (req, res, next) {
-  res.locals.asset_path="/public/";
-  next();
-});
+      var storedValue = req.session.data[name];
 
-// Add variables that are available in all views
-app.use(function (req, res, next) {
-  res.locals.serviceName=config.serviceName;
-  res.locals.cookieText=config.cookieText;
-  res.locals.releaseVersion="v" + releaseVersion;
-  next();
-});
+      if (storedValue == undefined){
+        return "";
+      }
 
-// Force HTTPs on production connections
-if (env === 'production' && useHttps === 'true'){
-  app.use(utils.forceHttps);
-}
+      if (Array.isArray(storedValue)){
 
-// Disallow search index idexing
-app.use(function (req, res, next) {
-  // Setting headers stops pages being indexed even if indexed pages link to them.
-  res.setHeader('X-Robots-Tag', 'noindex');
-  next();
-});
+        var inArray = storedValue.indexOf(value) != -1;
+        return inArray ? "checked" : "";
 
-app.get('/robots.txt', function (req, res) {
-  res.type('text/plain');
-  res.send("User-agent: *\nDisallow: /");
-});
+      } else {
 
-// routes (found in app/routes.js)
-if (typeof(routes) != "function"){
-  console.log(routes.bind);
-  console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.")
-  routes.bind(app);
-} else {
-  app.use("/", routes);
-}
+        return value == storedValue ? "checked" : "";
 
-// Strip .html and .htm if provided
-app.get(/\.html?$/i, function (req, res){
-  var path = req.path;
-  var parts = path.split('.');
-  parts.pop();
-  path = parts.join('.');
-  res.redirect(path);
-});
+      }
 
-// auto render any view that exists
-app.get(/^\/([^.]+)$/, function (req, res) {
+    });
 
-  var path = (req.params[0]);
+    next();
 
-  res.render(path, function(err, html) {
-    if (err) {
-      res.render(path + "/index", function(err2, html) {
-        if (err2) {
-          console.log(err);
-          res.status(404).send(err + "<br>" + err2);
-        } else {
-          res.end(html);
-        }
-      });
+  });
+
+  // Middleware to serve static assets
+  app.use('/public', express.static(__dirname + '/public'));
+  app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
+  app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
+  app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
+
+  // Elements refers to icon folder instead of images folder
+  app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
+
+  // Support for parsing data in POSTs
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  // Support session data
+  app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: Math.round(Math.random()*100000).toString()
+  }));
+
+  // send assetPath to all views
+  app.use(function (req, res, next) {
+    res.locals.asset_path="/public/";
+    next();
+  });
+
+  // Add variables that are available in all views
+  app.use(function (req, res, next) {
+    res.locals.serviceName=config.serviceName;
+    res.locals.cookieText=config.cookieText;
+    res.locals.releaseVersion="v" + releaseVersion;
+    next();
+  });
+
+  // Force HTTPs on production connections
+  if (env === 'production' && useHttps === 'true'){
+    app.use(utils.forceHttps);
+  }
+
+  // Disallow search index idexing
+  app.use(function (req, res, next) {
+    // Setting headers stops pages being indexed even if indexed pages link to them.
+    res.setHeader('X-Robots-Tag', 'noindex');
+    next();
+  });
+
+  app.use(utils.autoStoreData);
+
+  app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+  });
+
+  // routes (found in app/routes.js)
+  if (typeof(routes) != "function"){
+    console.log(routes.bind);
+    console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.")
+    routes.bind(app);
+  } else {
+    app.use("/", routes);
+  }
+
+  // Strip .html and .htm if provided
+  app.get(/\.html?$/i, function (req, res){
+    var path = req.path;
+    var parts = path.split('.');
+    parts.pop();
+    path = parts.join('.');
+    res.redirect(path);
+  });
+
+  // auto render any view that exists
+  app.get(/^\/([^.]+)$/, function (req, res) {
+
+    var path = (req.params[0]);
+
+    res.render(path, function(err, html) {
+      if (err) {
+        res.render(path + "/index", function(err2, html) {
+          if (err2) {
+            console.log(err);
+            res.status(404).send(err + "<br>" + err2);
+          } else {
+            res.end(html);
+          }
+        });
+      } else {
+        res.end(html);
+      }
+    });
+
+  });
+
+  // redirect all POSTs to GETs to avoid nasty refresh warning
+  app.post(/^\/([^.]+)$/, function(req, res){
+    res.redirect("/" + req.params[0]);
+  });
+
+  console.log("\nGOV.UK Prototype kit v" + releaseVersion);
+  // Display warning not to use kit for production services.
+  console.log("\nNOTICE: the kit is for building prototypes, do not use it for production services.");
+
+  // start the app
+  utils.findAvailablePort(app, function(port) {
+    console.log('Listening on port ' + port + '   url: http://localhost:' + port);
+    if (env === 'production') {
+      app.listen(port);
     } else {
-      res.end(html);
+      app.listen(port-50,function()
+      {
+        browserSync({
+          proxy:'localhost:'+(port-50),
+          port:port,
+          ui:false,
+          files:['public/**/*.*','app/views/**/*.*'],
+          ghostmode:false,
+          open:false,
+          notify:false,
+          logLevel: "error"
+        });
+      });
     }
   });
 
-});
 
-console.log("\nGOV.UK Prototype kit v" + releaseVersion);
-// Display warning not to use kit for production services.
-console.log("\nNOTICE: the kit is for building prototypes, do not use it for production services.");
-
-// start the app
-utils.findAvailablePort(app, function(port) {
-  console.log('Listening on port ' + port + '   url: http://localhost:' + port);
-  if (env === 'production') {
-    app.listen(port);
-  } else {
-    app.listen(port-50,function()
-    {
-      browserSync({
-        proxy:'localhost:'+(port-50),
-        port:port,
-        ui:false,
-        files:['public/**/*.*','app/views/**/*.*'],
-        ghostmode:false,
-        open:false,
-        notify:false,
-        logLevel: "error"
-      });
-    });
-  }
 });
