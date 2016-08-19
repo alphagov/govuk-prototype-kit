42be77447310c18154867c2955a666a4e462629c
Gemma Leigh <hello@gemmaleigh.com>
2016-08-19 13:29:52 +0100

    Use JavaScript Standard Style, remove semicolon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -107,7 +107,7 @@ $(document).ready(function () {
 
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
   // with role="button" when the space key is pressed.
-  GOVUK.shimLinksWithButtonRole.init();
+  GOVUK.shimLinksWithButtonRole.init()
 
   // Show and hide toggled content
   // Where .block-label uses the data-target attribute
