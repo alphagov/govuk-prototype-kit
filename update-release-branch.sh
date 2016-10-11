de0a3d37e4d3c598775f892d25ddc8ca7481c1cb
Travis CI <travis@travis-ci.org>
2016-10-11 11:15:57 +0100

    Amend latest release script
    - Remove the check for the version tag
    - Move git config inside if statement

--- update-release-branch.sh
+++ update-release-branch.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
 # Update the latest-release branch with the most recent tagged release
 
 # we want this to only happen from our repo, not forks
@@ -13,14 +10,15 @@ git config --global user.name "Travis CI"
 if [ "$TRAVIS_REPO_SLUG" == "alphagov/govuk_prototype_kit" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   # get the version from the version file
   VERSION_TAG="$(cat VERSION.txt)"
-  # check to make sure the tag exists
-  if git rev-parse "$VERSION_TAG" >/dev/null 2>&1; then
-    echo "Checking the most recent tag: $VERSION_TAG and creating a latest-release branch"
-    git remote add deploy-latest-release https://${GH_TOKEN}@github.com/alphagov/govuk_prototype_kit.git > /dev/null 2>&1
-    git checkout tags/"$VERSION_TAG" -b latest-release
-    git push -f deploy-latest-release latest-release
-    echo "Pushed latest-release branch to GitHub"
-  fi
+  echo "Using the most recent tag: $VERSION_TAG and creating a latest-release branch"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git remote add deploy-latest-release https://"${GH_TOKEN}"@github.com/alphagov/govuk_prototype_kit.git > /dev/null 2>&1
+  # check the remote has been added
+  git remote -v
+  git checkout -b latest-release v"$VERSION_TAG"
+  git push --force-with-lease deploy-latest-release latest-release
+  echo "Pushed latest-release branch to GitHub"
 else
   echo "Not updating the latest-release branch as we're on a branch..."
 fi
