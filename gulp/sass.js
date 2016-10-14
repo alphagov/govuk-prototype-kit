a9539af4268f0c80e05d255b7e8753aafe389193
Gemma Leigh <hello@gemmaleigh.com>
2016-10-14 12:51:03 +0100

    Compile the documentation sass

--- gulp/sass.js
+++ gulp/sass.js
@@ -21,3 +21,14 @@ gulp.task('sass', function () {
   .pipe(sourcemaps.write())
   .pipe(gulp.dest(config.paths.public + '/stylesheets/'))
 })
+
+gulp.task('sass-documentation', function () {
+  return gulp.src(config.paths.docsAssets + '/sass/*.scss')
+  .pipe(sass({outputStyle: 'expanded',
+              includePaths: ['govuk_modules/govuk_frontend_toolkit/stylesheets',
+                              'govuk_modules/govuk_template/assets/stylesheets',
+                              'govuk_modules/govuk-elements-sass/']}).on('error', sass.logError))
+  .pipe(sourcemaps.init())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(config.paths.public + '/stylesheets/'))
+})
