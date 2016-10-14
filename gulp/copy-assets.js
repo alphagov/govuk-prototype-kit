61839a42ce79855f0eac377519d3af4e523a9041
Gemma Leigh <hello@gemmaleigh.com>
2016-10-14 12:50:52 +0100

    Add a copy task for the documentation assets

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -11,3 +11,8 @@ gulp.task('copy-assets', function () {
   return gulp.src(['!' + config.paths.assets + 'sass{,/**/*}', config.paths.assets + '/**'])
   .pipe(gulp.dest(config.paths.public))
 })
+
+gulp.task('copy-documentation-assets', function () {
+  return gulp.src(['!' + config.paths.docsAssets + 'sass{,/**/*}', config.paths.docsAssets + '/**'])
+  .pipe(gulp.dest(config.paths.public))
+})
