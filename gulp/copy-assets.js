84f5999b0f629f6c4f4c2894f9d34670cee14067
Gemma Leigh <hello@gemmaleigh.com>
2016-10-14 16:23:58 +0100

    Add newline for readability

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -8,11 +8,13 @@ var gulp = require('gulp')
 var config = require('./config.json')
 
 gulp.task('copy-assets', function () {
-  return gulp.src(['!' + config.paths.assets + 'sass{,/**/*}', config.paths.assets + '/**'])
+  return gulp.src(['!' + config.paths.assets + 'sass{,/**/*}',
+    config.paths.assets + '/**'])
   .pipe(gulp.dest(config.paths.public))
 })
 
 gulp.task('copy-documentation-assets', function () {
-  return gulp.src(['!' + config.paths.docsAssets + 'sass{,/**/*}', config.paths.docsAssets + '/**'])
+  return gulp.src(['!' + config.paths.docsAssets + 'sass{,/**/*}',
+    config.paths.docsAssets + '/**'])
   .pipe(gulp.dest(config.paths.public))
 })
