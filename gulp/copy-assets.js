d28440ca48a252627ac8120fbfc5dffda977429f
Joe Lanman <joelanman@gmail.com>
2016-10-17 17:18:59 +0100

    Revert "Add a callback to the copy-assets task"
    This reverts commit cba3375756ff360a1c3e0092e632f2a90883532d.

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -7,10 +7,10 @@
 var gulp = require('gulp')
 var config = require('./config.json')
 
-gulp.task('copy-assets', function (cb) {
+gulp.task('copy-assets', function () {
   return gulp.src(['!' + config.paths.assets + 'sass{,/**/*}',
     config.paths.assets + '/**'])
-  .pipe(gulp.dest(config.paths.public)), cb
+  .pipe(gulp.dest(config.paths.public))
 })
 
 gulp.task('copy-documentation-assets', function () {
