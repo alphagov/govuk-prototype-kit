28b973b169ed04d614c23f7d290d72fc987ee11c
Joe Lanman <joelanman@gmail.com>
2016-10-20 11:25:58 +0100

    move gulp check to package.json, use gulp to remove .port.tmp

--- gulp/nodemon.js
+++ gulp/nodemon.js
@@ -4,6 +4,8 @@
   uses nodemon to run a server, watches for javascript and json changes
 */
 
+var fs = require('fs')
+var path = require('path')
 var gulp = require('gulp')
 var nodemon = require('gulp-nodemon')
 var config = require('./config.json')
@@ -15,5 +17,15 @@ gulp.task('server', function () {
     ignore: [config.paths.public + '*',
              config.paths.assets + '*',
              config.paths.nodeModules + '*']
+  }).on('quit', function(){
+
+    // remove .port.tmp if it exists
+    try {
+      fs.unlinkSync(path.join(__dirname, '/.port.tmp'))
+    } catch (e) {}
+
+    process.exit(0)
+
   })
+
 })
