7e24c9cb7a308f7b74d5e833ff2d29cd834e7661
Gemma Leigh <gemmaleigh@users.noreply.github.com>
2017-01-17 14:55:45 +0000

    Remove the title attribute
    To match the change made to GOV.UK elements: https://github.com/alphagov/govuk_elements/pull/389

--- app/config.js
+++ app/config.js
@@ -19,6 +19,6 @@ module.exports = {
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>'
 
 }
