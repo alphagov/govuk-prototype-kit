9eed33fb5449670cc4614dcb42cf65ea48c79fd5
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2017-02-05 21:44:59 +0000

    added success-fail-url example route

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,21 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+// Success/Failure Url redirect example
+router.post('/success-fail-url', function (req, res) {
+  var isSuccess = Boolean(req.body.success)
+  var isFailure = Boolean(req.body.failure)
+  var redirectUrl
+
+  if (isSuccess) {
+    redirectUrl = req.getSuccessUrl()
+  } else if (isFailure) {
+    redirectUrl = req.getFailUrl()
+  }
+
+  res.redirect(redirectUrl)
+})
+
 // add your routes here
 
 module.exports = router
