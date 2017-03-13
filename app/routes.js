006eb899a939d21540ceb34de663b16dbbbf6334
Sanjay Poyzer <sanjay.poyzer@digital.cabinet-office.gov.uk>
2017-03-13 16:41:01 +0000

    add a dry route for radio buttons pages

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,26 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+// Make pages with radio buttons named 'radio-group' route to their 'value' property
+
+router.get('*', function (req, res) {
+
+  //get the answer from the query string (eg. ?over18=false)
+  var radioGroup = req.query['radio-group'];
+
+  if (radioGroup) {
+
+    res.redirect(radioGroup);
+
+  } else {
+
+    // if radio-group is any other value (or is missing) render the page requested
+    var str = req.path;
+    res.render( str.substring(1) );
+
+  }
+});
+
 // add your routes here
 
 module.exports = router
