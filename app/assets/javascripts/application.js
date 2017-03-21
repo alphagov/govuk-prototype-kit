e0121960e2de0a34dec22431e627187b984ccb67
Gemma Leigh <hello@gemmaleigh.com>
2017-03-21 09:36:04 +0000

    Remove DEPRECATED selection-buttons.js
    This is no longer required with the radios and checkboxes released in
    govuk-elements-sass v3.0.0.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,6 @@ if (
 }
 
 $(document).ready(function () {
-  // Use GOV.UK selection-buttons.js to set selected
-  // and focused states for block labels
-  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
-  new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
-
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
   // with role="button" when the space key is pressed.
   GOVUK.shimLinksWithButtonRole.init()
