4055f9390023acc7bae063d0bbb7ea332d176e46
Edward Horsford <mail@edwardhorsford.com>
2017-04-05 11:30:32 +0100

    Add lesson 1 files

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Apply for a juggling licence',
 
   // Default port that prototype runs on
   port: '3000',
