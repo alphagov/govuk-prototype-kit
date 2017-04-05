ce90904e23fb568ec7b370f6542233d9141304de
Edward Horsford <mail@edwardhorsford.com>
2017-04-05 12:49:15 +0100

    Add lesson 4 files

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,21 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+router.post('/lesson-4/juggling-feat', function (req, res) {
+
+  // Make a variable and give it the value from 'juggling-ability'
+  var jugglingAbility = req.session.data['juggling-ability']
+
+  // Check whether the variable matches a condition
+  if (jugglingAbility == "3 or more"){
+    // Send user to next page
+    res.redirect('/lesson-4/juggling-feat')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/lesson-4/ineligible')
+  }
+
+})
+
 module.exports = router
