540dba0226dbcc905a67265dbe8f11efb3a9fffb
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-06-06 09:20:57 +0100

    Adding sticky back to contents link
    * Present on HTML publication
    * Was intended as part of recent testing but not ready in time, including now in case of future need

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,4 +49,118 @@ $(document).ready(function () {
       }, 0)
     }
   }
+
+  if ($('#contents').length) {
+    var test = new StickyElementContainer()
+    test.start($('#contentsSticky'))
+  }
 })
+
+StickyElementContainer = function () {
+  var self = this
+  // var $ = root.$
+  var $window = $(window)
+/*
+  var $ = root.$
+  var $window = $(root)
+*/
+  self._getWindowDimensions = function _getWindowDimensions () {
+    return {
+      height: $window.height(),
+      width: $window.width()
+    }
+  }
+
+  self._getWindowPositions = function _getWindowPositions () {
+    return {
+      scrollTop: $window.scrollTop()
+    }
+  }
+
+  self.start = function start ($el) {
+    var $element = $el.find('[data-sticky-element]')
+    var _hasResized = true
+    var _hasScrolled = true
+    var _interval = 50
+    var _windowVerticalPosition = 1
+    var _startPosition, _stopPosition
+
+    initialise()
+
+    function initialise () {
+      $window.resize(onResize)
+      $window.scroll(onScroll)
+      setInterval(checkResize, _interval)
+      setInterval(checkScroll, _interval)
+      checkResize()
+      checkScroll()
+      $element.addClass('govuk-sticky-element')
+    }
+
+    function onResize () {
+      _hasResized = true
+    }
+
+    function onScroll () {
+      _hasScrolled = true
+    }
+
+    function checkResize () {
+      if (_hasResized) {
+        _hasResized = false
+        _hasScrolled = true
+
+        var windowDimensions = self._getWindowDimensions()
+        _startPosition = $el.offset().top
+        _stopPosition = $el.offset().top + $el.height() - windowDimensions.height
+      }
+    }
+
+    function checkScroll () {
+      if (_hasScrolled) {
+        _hasScrolled = false
+
+        _windowVerticalPosition = self._getWindowPositions().scrollTop
+
+        updateVisibility()
+        updatePosition()
+      }
+    }
+
+    function updateVisibility () {
+      var isPastStart = _startPosition < _windowVerticalPosition
+      if (isPastStart) {
+        show()
+      } else {
+        hide()
+      }
+    }
+
+    function updatePosition () {
+      var isPastEnd = _stopPosition < _windowVerticalPosition
+      if (isPastEnd) {
+        stickToParent()
+      } else {
+        stickToWindow()
+      }
+    }
+
+    function stickToWindow () {
+      $element.addClass('govuk-sticky-element--stuck-to-window')
+      $element.removeClass('govuk-sticky-element--stuck-to-parent')
+    }
+
+    function stickToParent () {
+      $element.addClass('govuk-sticky-element--stuck-to-parent')
+      $element.removeClass('govuk-sticky-element--stuck-to-window')
+    }
+
+    function show () {
+      $element.removeClass('govuk-sticky-element--hidden')
+    }
+
+    function hide () {
+      $element.addClass('govuk-sticky-element--hidden')
+    }
+  }
+}
