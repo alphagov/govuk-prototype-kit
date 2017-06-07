d89f7eb7f9aa669a406de4d28114accb805f638b
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-06-07 16:53:24 +0100

    Added function to work out req field type

--- app/assets/javascripts/validation.js
+++ app/assets/javascripts/validation.js
@@ -4,5 +4,23 @@ $(document).on('submit', 'form', function (e) {
 
   if (reqFields.length > 0) {
     e.preventDefault()
+    for (var i = 0; i < reqFields.length; i++) {
+      var $formGroup = $(reqFields[i])
+      var type = findInputType($formGroup)
+      console.log(type)
+    }
   }
 })
+
+function findInputType ($formGroup) {
+  if ($formGroup.find('input[type="radio"]').length > 0) {
+    return 'radio'
+  }
+  if ($formGroup.find('input[type="checkbox"]').length > 0) {
+    return 'checkbox'
+  }
+  if ($formGroup.find('textarea').length > 0) {
+    return 'textarea'
+  }
+  return 'text'
+}
