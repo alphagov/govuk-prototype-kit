f5fac1033f09856481220250bf166dce15b9cc98
Chris Hanson <chris.hanson@digital.homeoffice.gov.uk>
2017-06-30 10:35:34 +0100

    Updated HOD components and patterns Sass to reflect work done on pattern library Added JS needed for tabs and table select patterns Removed the need for `hod__` prefix Updated index to reference new Sass folder structure

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,17 @@
+/* tab navigation and content show/hide */
+$('ul.tab--nav li a').click(function(){
+    var target = "#" + $(this).data("target");
+    $("ul.tab--nav li a").removeClass('active');
+    $(this).addClass('active');
+    $('.tab--content').not(target).addClass('js-hidden');
+    $(target).removeClass('js-hidden');
+});
+
+/* grey tab navigation and content show/hide */
+$('ul.tab--nav-grey li a').click(function(){
+    var target = "#" + $(this).data("target");
+    $("ul.tab--nav-grey li a").removeClass('active');
+    $(this).addClass('active');
+    $('.tab--content').not(target).addClass('js-hidden');
+    $(target).removeClass('js-hidden');
+});
