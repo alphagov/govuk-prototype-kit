7cf202f110e11b758fcb09e0d24a39ed764fe228
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2017-09-25 13:14:08 +0100

    Copying over updated assets for timeout warning from TEN repo.

--- app/assets/javascripts/modal-dialog.js
+++ app/assets/javascripts/modal-dialog.js
@@ -11,6 +11,7 @@
     $lastFocusedEl: null,
     $openButton: $('#openModal'),
     $closeButton: $('.modal-dialog .js-dialog-close'),
+    $cancelButton: $('.modal-dialog .js-dialog-cancel'),
     dialogIsOpenClass: 'dialog-is-open',
     timers: [],
     // Timer specific markup. If these are not present, timeout and redirection are disabled
@@ -22,14 +23,22 @@
     minutesTimeOutModalVisible: $('#js-modal-dialog').data('minutes-modal-visible'),
 
     bindUIElements: function () {
+       setTimeout(GOVUK.modalDialog.openDialog, 4000) //debug
+
       GOVUK.modalDialog.$openButton.on('click', function (e) {
         GOVUK.modalDialog.openDialog()
         return false
       })
+
       GOVUK.modalDialog.$closeButton.on('click', function (e) {
         e.preventDefault()
         GOVUK.modalDialog.closeDialog()
       })
+
+      GOVUK.modalDialog.$cancelButton.on('click', function (e) {
+        e.preventDefault()
+      })
+      // GOVUK.modalDialog.disableBackButtonWhenOpen()
     },
     isDialogOpen: function () {
       return GOVUK.modalDialog.el['open']
@@ -47,6 +56,10 @@
         if (GOVUK.modalDialog.isTimerSet()) {
           GOVUK.modalDialog.startTimer()
         }
+
+        // if (window.history.pushState) {
+        //   window.history.pushState('', '') // This updates the History API to enable state to be "popped" to detect browser navigation for disableBackButtonWhenOpen
+        // }
       }
     },
     closeDialog: function () {
@@ -60,6 +73,15 @@
           GOVUK.modalDialog.clearTimers()
         }
       }
+    },
+    disableBackButtonWhenOpen: function () {
+      window.addEventListener('popstate', function () {
+        if (GOVUK.modalDialog.isDialogOpen()) {
+          GOVUK.modalDialog.closeDialog()
+        } else {
+          window.history.go(-1)
+        }
+      })
     },
     saveLastFocusedEl: function () {
       GOVUK.modalDialog.$lastFocusedEl = document.activeElement
@@ -82,6 +104,7 @@
     makePageContentInert: function () {
       if (document.querySelector('#content')) {
         document.querySelector('#content').inert = true
+        document.querySelector('#content').setAttribute('aria-hidden', 'true')
       }
     },
     // Make page content active when modal is not open
@@ -89,6 +112,7 @@
     removeInertFromPageContent: function () {
       if (document.querySelector('#content')) {
         document.querySelector('#content').inert = false
+        document.querySelector('#content').setAttribute('aria-hidden', 'false')
       }
     },
     // Starts a timer. If modal not closed before time out + 4 seconds grace period, user is redirected.
@@ -97,6 +121,8 @@
       var $timer = GOVUK.modalDialog.$timer
       var $accessibleTimer = GOVUK.modalDialog.$accessibleTimer
       var minutes = GOVUK.modalDialog.minutesTimeOutModalVisible
+      var timerRunOnce = false
+      var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
 
       if (minutes) {
         var seconds = 60 * minutes
@@ -108,43 +134,70 @@
           var secondsLeft = parseInt(seconds % 60, 10)
           var timerExpired = minutesLeft < 1 && secondsLeft < 1
 
-          var minutesText = minutesLeft > 0 ? minutesLeft + ' minute' + (minutesLeft > 1 ? 's' : '') + ' ' : ''
-          var secondsText = secondsLeft >= 1 ? secondsLeft + ' second' + (secondsLeft > 1 ? 's' : '') + ' ' : ''
+          var minutesText = minutesLeft > 0 ? minutesLeft + ' minute' + (minutesLeft > 1 ? 's' : '') + '' : ' '
+          var secondsText = secondsLeft >= 1 ? ' ' + secondsLeft + ' second' + (secondsLeft > 1 ? 's' : '') + '' : ''
+          var atMinutesNumberAsText = ''
+          var atSecondsNumberAsText = ''
 
-          var text = 'You will be redirected in ' + minutesText + secondsText
+          try {
+            atMinutesNumberAsText = GOVUK.modalDialog.numberToWords(minutesLeft) // Attempt to convert numerics into text as iOS VoiceOver ccassionally stalled when encountering numbers
+            atSecondsNumberAsText = GOVUK.modalDialog.numberToWords(secondsLeft)
+          } catch (e) {
+            atMinutesNumberAsText = minutesLeft
+            atSecondsNumberAsText = secondsLeft
+          }
+
+          var atMinutesText = minutesLeft > 0 ? atMinutesNumberAsText + ' minute' + (minutesLeft > 1 ? 's' : '') + '' : ''
+          var atSecondsText = secondsLeft >= 1 ? ' ' + atSecondsNumberAsText + ' second' + (secondsLeft > 1 ? 's' : '') + '' : ''
+
+          // Below string will get read out by screen readers every time the timeout refreshes (every 15 secs. See below).
+          // Please add additional information in the modal body content or in below extraText which will get announced to AT the first time the time out opens
+          var text = 'We will reset your application if you do not respond in ' + minutesText + secondsText + '.'
+          var atText = 'We will reset your application if you do not respond in ' + atMinutesText
+          if (atSecondsText) {
+            if (minutesLeft > 0) {
+              atText += ' and'
+            }
+            atText += atSecondsText + '.'
+          } else {
+            atText += '.'
+          }
+          var extraText = ' We do this to keep your information secure.'
 
           if (timerExpired) {
             $timer.text('You are about to be redirected')
             $accessibleTimer.text('You are about to be redirected')
             setTimeout(GOVUK.modalDialog.redirect, 4000)
           } else {
-            $timer.text(text)
-
             seconds--
 
-            if (minutesLeft < 1) {
-               // If less than 20 seconds left, make aria-live assertive and update content every 5 secs
-              if (secondsLeft < 20) {
-                $accessibleTimer.attr('aria-live', 'assertive')
+            $timer.text(text + extraText)
 
-                if (secondsLeft % 5 === 0) {
-                  $accessibleTimer.text(text)
-                }
-              } else if (secondsLeft % 20 === 0) {
-                // If less than 1 minute left, update screen reader friendly content every 20 secs
-                $accessibleTimer.text(text)
-              }
-            } else if (secondsLeft % 30 === 0) {
-              // Update screen reader friendly content every 30 secs
-              $accessibleTimer.text(text)
+            if (minutesLeft < 1 && secondsLeft < 20) {
+              $accessibleTimer.attr('aria-live', 'assertive')
             }
-            // JS won't doesn't allow resetting timers globally so timers need to be retained for resetting.
+
+            if (!timerRunOnce) {
+              // Read out the extra content only once. Don't read out on iOS VoiceOver which stalls on the longer text
+
+              if (iOS) {
+                $accessibleTimer.text(atText)
+              } else {
+                $accessibleTimer.text(atText + extraText)
+              }
+              timerRunOnce = true
+            } else if (secondsLeft % 15 === 0) {
+              // Update screen reader friendly content every 15 secs
+              $accessibleTimer.text(atText)
+            }
+
+            // JS doesn't allow resetting timers globally so timers need to be retained for resetting.
             GOVUK.modalDialog.timers.push(setTimeout(runTimer, 1000))
           }
         })()
       }
     },
-    // Clears modal timer.
+    // Clears modal timer
     clearTimers: function () {
       for (var i = 0; i < GOVUK.modalDialog.timers.length; i++) {
         clearTimeout(GOVUK.modalDialog.timers[i])
@@ -173,6 +226,7 @@
         window.onclick = resetTimer     // Catches touchpad clicks
         window.onscroll = resetTimer    // Catches scrolling with arrow keys
         window.onkeypress = resetTimer
+        window.onkeyup = resetTimer // Catches Android keypad presses
       }
 
       function resetTimer () {
@@ -182,6 +236,85 @@
     },
     redirect: function () {
       window.location.replace(GOVUK.modalDialog.timeOutRedirectUrl)
+    },
+    numberToWords: function (n) {
+      var string = n.toString()
+      var units
+      var tens
+      var scales
+      var start
+      var end
+      var chunks
+      var chunksLen
+      var chunk
+      var ints
+      var i
+      var word
+      var words = 'and'
+
+      if (parseInt(string) === 0) {
+        return 'zero'
+      }
+
+      /* Array of units as words */
+      units = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ]
+
+      /* Array of tens as words */
+      tens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ]
+
+      /* Array of scales as words */
+      scales = [ '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion' ]
+
+      /* Split user arguemnt into 3 digit chunks from right to left */
+      start = string.length
+      chunks = []
+      while (start > 0) {
+        end = start
+        chunks.push(string.slice((start = Math.max(0, start - 3)), end))
+      }
+
+      /* Check if function has enough scale words to be able to stringify the user argument */
+      chunksLen = chunks.length
+      if (chunksLen > scales.length) {
+        return ''
+      }
+
+      /* Stringify each integer in each chunk */
+      words = []
+      for (i = 0; i < chunksLen; i++) {
+        chunk = parseInt(chunks[i])
+
+        if (chunk) {
+          /* Split chunk into array of individual integers */
+          ints = chunks[i].split('').reverse().map(parseFloat)
+
+          /* If tens integer is 1, i.e. 10, then add 10 to units integer */
+          if (ints[1] === 1) {
+            ints[0] += 10
+          }
+
+          /* Add scale word if chunk is not zero and array item exists */
+          if ((word = scales[i])) {
+            words.push(word)
+          }
+
+          /* Add unit word if array item exists */
+          if ((word = units[ints[0]])) {
+            words.push(word)
+          }
+
+          /* Add tens word if array item exists */
+          if ((word = tens[ ints[1] ])) {
+            words.push(word)
+          }
+
+          /* Add hundreds word if array item exists */
+          if ((word = units[ints[2]])) {
+            words.push(word + ' hundred')
+          }
+        }
+      }
+      return words.reverse().join(' ')
     },
     init: function () {
       if (GOVUK.modalDialog.el) {
