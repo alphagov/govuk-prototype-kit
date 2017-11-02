d94f9d11297dfd30045b78750c0299f99945d51a
Joe Lanman <joelanman@gmail.com>
2017-11-02 12:29:53 +0000

    remove elements and frontend toolkit, use new frontend

--- server.js
+++ server.js
@@ -78,12 +78,12 @@ app.set('view engine', 'html')
 
 // Middleware to serve static assets
 app.use('/public', express.static(path.join(__dirname, '/public')))
-app.use('/public', express.static(path.join(__dirname, '/govuk_modules/govuk_template/assets')))
-app.use('/public', express.static(path.join(__dirname, '/govuk_modules/govuk_frontend_toolkit')))
-app.use('/public/images/icons', express.static(path.join(__dirname, '/govuk_modules/govuk_frontend_toolkit/images')))
+app.use('/public', express.static(path.join(__dirname, '/node_modules/govuk_template_jinja/assets')))
+// app.use('/public', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit')))
+// app.use('/public/images/icons', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit/images')))
 
 // Elements refers to icon folder instead of images folder
-app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'node_modules', 'govuk_template_jinja', 'assets', 'images', 'favicon.ico')))
 
 // Set up documentation app
 if (useDocumentation) {
