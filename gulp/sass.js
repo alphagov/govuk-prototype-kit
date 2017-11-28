a604894778f51fad19f7576d667b34b97c857b04
Joe Lanman <joelanman@gmail.com>
2017-11-28 19:20:55 +0000

    standardJS fixes

--- gulp/sass.js
+++ gulp/sass.js
@@ -15,7 +15,7 @@ gulp.task('sass', function () {
   return gulp.src(config.paths.assets + '/sass/*.scss')
   .pipe(sourcemaps.init())
   .pipe(sass({outputStyle: 'expanded',
-    includePaths: [/*'govuk_modules/govuk_frontend_toolkit/stylesheets',*/
+    includePaths: [/* 'govuk_modules/govuk_frontend_toolkit/stylesheets', */
       'node_modules/govuk_template_jinja/assets/stylesheets',
       'node_modules']}).on('error', sass.logError))
   .pipe(sourcemaps.write())
@@ -26,7 +26,7 @@ gulp.task('sass-documentation', function () {
   return gulp.src(config.paths.docsAssets + '/sass/*.scss')
   .pipe(sourcemaps.init())
   .pipe(sass({outputStyle: 'expanded',
-    includePaths: [/*'govuk_modules/govuk_frontend_toolkit/stylesheets',*/
+    includePaths: [/* 'govuk_modules/govuk_frontend_toolkit/stylesheets', */
       'node_modules/govuk_template_jinja/assets/stylesheets',
       'node_modules']}).on('error', sass.logError))
   .pipe(sourcemaps.write())
