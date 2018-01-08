330abbaa6c2c873a10905db3de4c9c0e7009b519
Mike Shaw <mike.shaw@dwp.gsi.gov.uk>
2018-01-08 16:42:19 +0000

    Add explanatory comment to defaults file

--- app/defaults.js
+++ app/defaults.js
@@ -1,3 +1,9 @@
+/**
+ * Provide default values for the user session. These are automatically added
+ * via the `autoStoreData` middleware. These values will only be added to the
+ * session, if a value doesn't already exist.
+ * @type {Object}
+ */
 module.exports = {
 
 };
