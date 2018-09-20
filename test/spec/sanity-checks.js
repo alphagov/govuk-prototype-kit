9a26289e5b1e315c3ffa5ba65b02f9d0b80164e1
Mat Carey <mat+yagni-digital@matcarey.co.uk>
2018-09-20 12:42:53 +0100

    Adding tests to support upcoming changes.

--- test/spec/sanity-checks.js
+++ test/spec/sanity-checks.js
@@ -5,6 +5,10 @@ var path = require('path')
 var fs = require('fs')
 var assert = require('assert')
 
+function readFile (pathFromRoot) {
+  return fs.readFileSync(path.join(__dirname, '../../' + pathFromRoot), 'utf8')
+}
+
 /**
  * Basic sanity checks on the dev server
  */
@@ -54,12 +58,28 @@ describe('The Prototype Kit', function () {
         if (err) {
           done(err)
         } else {
+          assert.equal(res.text, readFile('node_modules/govuk-frontend/all.js'))
           done()
         }
       })
   })
 
-  it('should send with a well formed response for the docs page', function (done) {
+  it('should allow known assets to be loaded from node_modules', function (done) {
+    request(app)
+      .get('/assets/images/favicon.ico')
+      .expect('Content-Type', /image\/x-icon/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          done(err)
+        } else {
+          assert.equal(res.body, readFile('node_modules/govuk-frontend/assets/images/favicon.ico'))
+          done()
+        }
+      })
+  })
+
+  it('should not expose everything', function (done) {
     request(app)
       .get('/extension-assets/govuk-frontend/common.js')
       .expect(404)
