287e0a98f57061797463fd146eebf883078301c7
Ernest Walzel <ewalzel@gmail.com>
2018-11-22 16:16:09 +0100

    Zalozenie zivnosti -- index

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Založenie živnosti',
+  serviceName: 'Návody.digital',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/zalozenie-zivnosti/:pageUrl', function (req, res) {
+  if (req.params.pageUrl == null) return res.render('zalozenie-zivnosti/index.html')
+})
 
 module.exports = router
