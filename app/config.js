495abb08a8be47f0446fabce24bc160354bd5c84
Ernest Walzel <ewalzel@gmail.com>
2018-11-22 15:09:50 +0100

    Basic config

--- app/config.js
+++ app/config.js
@@ -4,13 +4,13 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Založenie živnosti',
 
   // Default port that prototype runs on
   port: '3000',
 
   // Enable or disable password protection on production
-  useAuth: 'true',
+  useAuth: 'false',
 
   // Automatically stores form data, and send to all views
   useAutoStoreData: 'true',
