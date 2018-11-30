623f0fd5fc3ce762a7d67ef80b42868fc2912856
Ernest Walzel <ewalzel@gmail.com>
2018-11-30 20:38:10 +0100

    Fix linking

--- app/zalozenie_zivnosti_routes.js
+++ app/zalozenie_zivnosti_routes.js
@@ -10,12 +10,12 @@ router.get('/bankovy-ucet', function (req, res) {
   res.render('zalozenie-zivnosti/bankovy_ucet.html', buildRequestData(req))
 })
 
-// Ohlasenie
-router.use('/ohlasenie', ohlasenieRouter)
-ohlasenieRouter.get('/', function (req, res) {
+router.get('/ohlasenie', function (req, res) {
   res.render('zalozenie-zivnosti/ohlasenie.html', buildRequestData(req))
 })
 
+// Ohlasenie
+router.use('/ohlasenie/?', ohlasenieRouter)
 ohlasenieRouter.get('/start', function (req, res) {
   res.render('zalozenie-zivnosti/ohlasenie/osobne_udaje.html', buildRequestData(req))
 })
@@ -54,58 +54,41 @@ ohlasenieRouter.get('/zhrnutie', function (req, res) {
 
 function buildRequestData (request) {
   function urlTo (path) {
-    return '/zalozenie-zivnosti' + '/' + path
+    return '/zalozenie-zivnosti' + path
   }
 
+  console.log(request.path, request.originalUrl, request.baseUrl + request.path)
   return {
     'serviceName': 'Založenie živnosti: krok po kroku',
     'serviceUrl': '/zalozenie-zivnosti',
-    'activeHref': request.baseUrl + request.path.slice(0, -2),
     'navigationSteps': [
       {
         number: '0',
         title: 'Úvod',
         description: 'Nepovinné, ale užitočné. Postačí vám aj bežný účet.',
-        href: urlTo('')
+        href: urlTo('/'),
+        isActive: request.path === '/'
       },
       {
         number: '1',
         title: 'Založte si bankový účet',
         description: 'Nepovinné, ale užitočné. Postačí vám aj bežný účet.',
-        href: urlTo('bankovy-ucet')
+        href: urlTo('/bankovy-ucet'),
+        isActive: request.path === '/bankovy-ucet'
       },
       {
         number: '2',
         title: 'Ohláste svoju živnosť',
         description: 'Zaregistrujte vašu živnosť a získajte IČO.',
-        href: urlTo('ohlasenie')
+        href: urlTo('/ohlasenie'),
+        isActive: request.path === '/ohlasenie'
       },
       {
         number: '3',
         title: 'Zaregistrujte sa na DPH',
         description: 'Ak budete pracovať pre zahraničných zákazníkov alebo obracať veľké peniaze.',
-        href: 'dph',
-        links: [
-          {
-            text: 'Zistite, či sa potrebujete zaregistrovať na DPH',
-            href: '#'
-          }
-        ]
-      },
-      {
-        number: '4',
-        title: 'Dane a odvody: čo treba platiť',
-        description: 'Kedy a či začať platiť zdravotné a sociálne poistenie.',
-        links: [
-          {
-            text: 'Zistite, aké odvody musíte platiť',
-            href: '#'
-          },
-          {
-            text: 'Dôležité termíny pre platenie daní',
-            href: '#'
-          }
-        ]
+        href: urlTo('/dph'),
+        isActive: request.path === '/dph'
       }
     ]
   }
