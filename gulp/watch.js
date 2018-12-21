2bc90d0bc6890be7acc6a57a6b59fb6640eedeab
Ollie Williams <owilliams@companieshouse.gov.uk>
2018-12-21 08:54:19 +0000

    Update watch.js
    - replace gulp.series on watch tasks with gulp.task

--- gulp/watch.js
+++ gulp/watch.js
@@ -9,21 +9,21 @@ const gulp = require('gulp')
 const config = require('./config.json')
 
 gulp.task('watch-sass', function () {
-  return gulp.watch(config.paths.assets + 'sass/**', { cwd: './' }, gulp.series('sass'))
+  return gulp.watch(config.paths.assets + 'sass/**', { cwd: './' }, gulp.task('sass'))
 })
 
 gulp.task('watch-assets', function () {
   return gulp.watch([config.paths.assets + 'images/**',
-    config.paths.assets + 'javascripts/**'], { cwd: './' }, gulp.series('copy-assets'))
+    config.paths.assets + 'javascripts/**'], { cwd: './' }, gulp.task('copy-assets'))
 })
 
 // Backward compatibility with Elements
 
 gulp.task('watch-sass-v6', function () {
-  return gulp.watch(config.paths.v6Assets + 'sass/**', { cwd: './' }, gulp.series('sass-v6'))
+  return gulp.watch(config.paths.v6Assets + 'sass/**', { cwd: './' }, gulp.task('sass-v6'))
 })
 
 gulp.task('watch-assets-v6', function () {
   return gulp.watch([config.paths.v6Assets + 'images/**',
-    config.paths.v6Assets + 'javascripts/**'], { cwd: './' }, gulp.series('copy-assets-v6'))
+    config.paths.v6Assets + 'javascripts/**'], { cwd: './' }, gulp.task('copy-assets-v6'))
 })
