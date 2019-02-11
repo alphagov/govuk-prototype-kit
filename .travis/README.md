f481da1a019131594048940a9e77b23e8a56b81f
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2019-02-11 12:17:12 +0000

    Update documentation for create-release

--- .travis/README.md
+++ .travis/README.md
@@ -1,18 +1,48 @@
 # Travis encrypted files
 
-This directory contains a public/private keypair generated just for this repository.
+This directory contains an encrypted private deploy key with write access to the
+Prototype Kit repository.
 
-The public key is a deploy key which has been added to the GitHub repo for push access.
+It has been encrypted using a key stored in the DEPLOY_KEY environment
+variable, which is itself encrypted using `travis encrypt`.
 
-The private key is encrypted using `travis encrypt-file` and then committed to this repo.
+The deploy key is decrypted in create-release.sh.
 
-The decrypt commands are in `.travis.yml`.
+To update the key:
 
-To regenerate a key:
+1. Generate a new keypair using ssh-keygen
+   
+  ```
+  ssh-keygen -b 4096 -f .travis/prototype-kit-deploy-key
+  ```
 
-```
-ssh-keygen -b 4096 -f .travis/govuk_prototype_kit   # Make a new keypair
-travis encrypt-file .travis/govuk_prototype_kit     # Encrypt the private key
-mv govuk_prototype_kit.enc .travis/                 # Move the private key to the right place
-rm .travis/govuk_prototype_kit                      # Remove the unencrypted private key
-```
+2. Add the *public* key as a new [deploy key], with write access to the
+   repository
+
+  ```
+  cat .travis/prototype-kit-deploy-key.pub
+  ```
+
+2. Generate a new random string which we can use as an encryption key
+
+3. Encrypt the private key using ssh-keygen
+
+  ```
+  openssl aes-256-cbc -k [encryption key here] \
+    -in prototype-kit-deploy-key \
+    -out prototype-kit-deploy-key.enc
+  ```
+
+4. Remove the unencrypted private key and the public key
+
+5. Encrypt the private key using the encryption key
+
+  ```
+  travis encrypt DEPLOY_KEY=[encryption key]
+  ```
+
+6. Add the encrypted variable to the environment variables for the deploy job
+   in .travis.yml
+
+
+[deploy key]: https://github.com/alphagov/govuk-prototype-kit/settings/keys
--- create-release.sh
+++ create-release.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
-REPO_PATH='alphagov/govuk-prototype-kit'
+# Check for the TRAVIS environment variable
+if [[ -z "${TRAVIS}" ]]; then
+  echo "⛔️ Refusing to run outside of Travis..."
+  exit 1
+fi
 
-echo "Adding config for $REPO_PATH"
-
+# Configure git...
 git config --global user.name "Travis CI"
 git config --global user.email "travis@travis-ci.org"
-git remote add origin_ssh git@github.com:$REPO_PATH.git
+git remote add origin_ssh git@github.com:alphagov/govuk-prototype-kit.git
 
+# Decrypt deploy key.
+# 
 # See `.travis/README.md` for more details
 openssl aes-256-cbc -d -k $DEPLOY_KEY \
   -in .travis/prototype-kit-deploy-key.enc \
@@ -17,31 +22,30 @@ openssl aes-256-cbc -d -k $DEPLOY_KEY \
 chmod 600 ~/.ssh/id_rsa
 
 # Verify that we can authenticate with GitHub
+#
+# If this is working correctly, you should see this in the build output:
+#
+# "Hi alphagov/govuk-prototype-kit! You've successfully authenticated, but
+# GitHub does not provide shell access."
+echo "👋 Testing that we can authenticate with GitHub..."
 ssh -T git@github.com
-
-echo "Checking to see if the version file has been updated"
 
 # Get the version from the version file
 VERSION_TAG="v`cat VERSION.txt`"
 
-# Create a new tag - if the version file has been updated and a tag for that
-# version doesn't already exist
-
-# Check to make sure the tag doesn't already exist
+# Check that there's not a tag for the current version already
 if ! git rev-parse $VERSION_TAG >/dev/null 2>&1; then
-  echo "Creating new tag: $VERSION_TAG"
-
-  # Create a new tag and push to Github
+  # Create a new tag and push to GitHub.
+  # 
+  # GitHub will automatically create a release for the tag, ignoring any files
+  # specified in the .gitattributes file
+  echo "🏷 Creating new tag: $VERSION_TAG"
   git tag $VERSION_TAG
   git push origin_ssh $VERSION_TAG
 
-  # This tag will trigger the builds for the deploy providers marked "# For tagged commits" in .travis.yml
-
-  # Alias branch for the most recently released tag, for easier diffing
-  # Force push local `master` branch to the `latest-release` branch on Github
+  # Force push the latest-release branch to this commit
+  echo "💨 Pushing latest-release branch to GitHub"
   git push --force origin_ssh master:latest-release
-  echo "Pushed latest-release branch to GitHub"
-
 else
-  echo "Not creating a new tag, or updating the latest-release branch as the tag already exists..."
+  echo "😴 Current version already exists as a tag on GitHub. Nothing to do."
 fi
