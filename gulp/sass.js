1845bf28c88e921507e9d28550fa8ade83020be6
Mat Carey <mat@dorightdigital.com>
2019-02-13 17:30:52 +0000

    Fixed extensions for windows (including tests).

--- gulp/sass.js
+++ gulp/sass.js
@@ -15,8 +15,8 @@ const extensions = require('../lib/extensions/extensions')
 const config = require('./config.json')
 
 gulp.task('sass-extensions', function (done) {
-  const fileContents = '$govuk-extensions-url-context: "/extension-assets";' + extensions.getFileSystemPaths('sass')
-    .map(filePath => `@import "${filePath}";`)
+  const fileContents = '$govuk-extensions-url-context: "/extension-assets"; ' + extensions.getFileSystemPaths('sass')
+    .map(filePath => `@import "${filePath.split(path.sep).join('/')}";`)
     .join('\n')
   fs.writeFile(path.join(config.paths.lib + 'extensions', '_extensions.scss'), fileContents, done)
 })
