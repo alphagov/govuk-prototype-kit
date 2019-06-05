dbce0679b970664edaa8a9d8867c3779e6b85be5
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2019-06-05 13:24:15 +0100

    SPIKE updating to pre-release namespace changes

--- __tests__/spec/sanity-checks.js
+++ __tests__/spec/sanity-checks.js
@@ -54,7 +54,7 @@ describe('The Prototype Kit', () => {
           if (err) {
             done(err)
           } else {
-            assert.strictEqual('' + res.text, readFile('node_modules/govuk-frontend/all.js'))
+            assert.strictEqual('' + res.text, readFile('node_modules/govuk-frontend/govuk/all.js'))
             done()
           }
         })
@@ -69,7 +69,7 @@ describe('The Prototype Kit', () => {
           if (err) {
             done(err)
           } else {
-            assert.strictEqual('' + res.body, readFile('node_modules/govuk-frontend/assets/images/favicon.ico'))
+            assert.strictEqual('' + res.body, readFile('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'))
             done()
           }
         })
@@ -91,14 +91,14 @@ describe('The Prototype Kit', () => {
     describe('misconfigured prototype kit - while upgrading kit developer did not copy over changes in /app folder', () => {
       it('should still allow known assets to be loaded from node_modules', (done) => {
         request(app)
-          .get('/node_modules/govuk-frontend/all.js')
+          .get('/node_modules/govuk-frontend/govuk/all.js')
           .expect('Content-Type', /application\/javascript; charset=UTF-8/)
           .expect(200)
           .end(function (err, res) {
             if (err) {
               done(err)
             } else {
-              assert.strictEqual('' + res.text, readFile('node_modules/govuk-frontend/all.js'))
+              assert.strictEqual('' + res.text, readFile('node_modules/govuk-frontend/govuk/all.js'))
               done()
             }
           })
--- server.js
+++ server.js
@@ -117,8 +117,8 @@ app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/no
 // Set up documentation app
 if (useDocumentation) {
   var documentationViews = [
-    path.join(__dirname, '/node_modules/govuk-frontend/'),
-    path.join(__dirname, '/node_modules/govuk-frontend/components'),
+    path.join(__dirname, '/node_modules/govuk-frontend/govuk/'),
+    path.join(__dirname, '/node_modules/govuk-frontend/govuk/components'),
     path.join(__dirname, '/docs/views/'),
     path.join(__dirname, '/lib/')
   ]
