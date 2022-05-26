7afac388e5c93e06e8cef9739b4aab1fad60f835
Joe Lanman <joe.lanman@digital.cabinet-office.gov.uk>
2022-05-26 14:33:16 +0100

    revert to exit(0)
    nicer experience on older version of npm - no stack trace

--- __tests__/spec/start.js
+++ __tests__/spec/start.js
@@ -1,12 +1,11 @@
 /* eslint-env jest */
 
 const child_process = require('child_process') // eslint-disable-line camelcase
-const fs = require('fs')
 const path = require('path')
 
 const utils = require('./utils')
 
-describe('npm run start', () => {
+describe('npm start', () => {
   const tmpDir = utils.mkdtempSync()
 
   describe('prestart', () => {
@@ -14,15 +13,9 @@ describe('npm run start', () => {
       const testDir = path.join(tmpDir, 'check-node-modules-exists')
       utils.mkPrototypeSync(testDir)
 
-      expect(() => {
-        child_process.execSync('npm run start', { cwd: testDir })
-      }).toThrow(/ERROR: Node module folder missing. Try running `npm install`\n$/)
-
-      fs.mkdirSync(path.join(testDir, 'node_modules'))
-
-      expect(() => {
-        child_process.execSync('npm run predev', { cwd: testDir })
-      }).not.toThrow()
+      expect(
+        child_process.execSync('npm start', { cwd: testDir, encoding: 'utf8' })
+      ).toMatch(/ERROR: Node module folder missing. Try running `npm install`\n$/)
     })
   })
 })
