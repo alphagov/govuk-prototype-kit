1999754ff0e100e3fd1ba0f850ac4c85e41eaaaa
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-06-14 17:23:19 +0100

    Tell Jest to expect install test to fail if using an old version of npm
    For npm@6 and older you have to use a different option to tell it to not
    install optional dependencies. Unfortunately, adding that option to our
    npmrc results in warnings for npm@8.6 and above. So instead we just
    don't support npm@6 (see issue #1354). This means the tests will fail on
    Node 12 and 14 (which come with npm@6 by default), so we mark the test
    as expected to fail when npm is old.

--- __tests__/spec/install.js
+++ __tests__/spec/install.js
@@ -6,10 +6,23 @@ const path = require('path')
 
 const utils = require('./utils')
 
+const npmVersion = child_process.execSync(
+  'npm -v', { encoding: 'utf8' }
+).trim().split('.')
+
+function testFailingIf (condition, ...args) {
+  if (condition) {
+    return test.failing(...args)
+  } else {
+    return test(...args)
+  }
+}
+
 describe('npm install', () => {
   const tmpDir = utils.mkdtempSync()
 
-  it('does not install dev dependencies by default', () => {
+  // TODO: This test can be made unconditional when we drop support for Node 14
+  testFailingIf(npmVersion[0] < 8, 'does not install dev dependencies by default', () => {
     const testDir = path.join(tmpDir, 'install-no-optional')
     utils.mkPrototypeSync(testDir)
 
