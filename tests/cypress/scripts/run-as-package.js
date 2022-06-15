dc9dfe8ef4d047114ca5926389df892f19acd9f6
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-06-15 09:08:18 +0100

    Update paths

--- tests/cypress/scripts/run-as-package.js
+++ tests/cypress/scripts/run-as-package.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const fse = require('fs-extra')
 const path = require('path')
 
-const prototypePkg = require('../../package.json')
-const utils = require('../../__tests__/spec/utils')
+const prototypePkg = require('../../../package.json')
+const utils = require('../../spec/utils')
 
 const testDir = path.resolve(process.env.KIT_TEST_DIR || 'cypress/temp/test-project')
 const releaseArchive = utils.mkReleaseArchiveSync({ archiveType: 'tar', dir: path.resolve('cypress', 'temp') })
