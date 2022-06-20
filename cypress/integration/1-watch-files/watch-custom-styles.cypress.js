468279439bb6ba4224b9a4d978dd2725a3748551
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2022-06-20 11:18:27 +0100

    Integration test for arbitrary sass changes

---
+++ cypress/integration/1-watch-files/watch-custom-styles.cypress.js
@@ -0,0 +1,60 @@
+const path = require('path')
+
+const { waitForApplication } = require('../utils')
+
+const customStyleFile = path.join(Cypress.env('projectFolder'), 'app', 'assets', 'sass', 'custom.scss')
+const appHeadView = path.join(Cypress.env('projectFolder'), 'app', 'views', 'includes', 'head.html')
+const backupHeadView = path.join(Cypress.env('tempFolder'), 'temp-head.html')
+const publicStylesheet = 'public/stylesheets/custom.css'
+
+const GREEN = 'rgb(0, 255, 0)'
+const BLACK = 'rgb(11, 12, 12)'
+
+describe('watch custom sass files', () => {
+  describe(`sass file ${customStyleFile} should be created and linked within ${appHeadView} and accessible from the browser as /${publicStylesheet}`, () => {
+    const cssStatement = `
+    .govuk-header { background: ${GREEN}; }
+    `
+
+    before(() => {
+      waitForApplication()
+      // backup head view
+      cy.task('copyFile', { source: appHeadView, target: backupHeadView })
+    })
+
+    afterEach(() => {
+      // restore head view
+      cy.task('copyFile', { source: backupHeadView, target: appHeadView })
+
+      // delete temporary files
+      cy.task('deleteFile', { filename: path.join(Cypress.env('projectFolder'), publicStylesheet) })
+      cy.task('deleteFile', { filename: customStyleFile })
+      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', BLACK)
+      cy.task('deleteFile', { filename: backupHeadView })
+    })
+
+    it('The colour of the header should be changed to green then back to black', () => {
+      cy.task('log', 'The colour of the header should be black')
+      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', BLACK)
+
+      cy.task('log', `Create ${customStyleFile}`)
+      cy.task('createFile', {
+        filename: customStyleFile,
+        data: cssStatement
+      })
+
+      cy.wait(6000)
+
+      cy.task('log', `Amend ${appHeadView} to link ${publicStylesheet}`)
+      cy.task('appendFile', {
+        filename: appHeadView,
+        data: `
+  <link href="/${publicStylesheet}" rel="stylesheet" type="text/css" />
+      `
+      })
+
+      cy.task('log', 'The colour of the header should be changed to green')
+      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', GREEN)
+    })
+  })
+})
--- cypress/integration/1-watch-files/watch-styles.cypress.js
+++ cypress/integration/1-watch-files/watch-styles.cypress.js
@@ -9,6 +9,9 @@ const cypressTestStylePattern = `${appStyles}/patterns/_${cypressTestStyles}.scs
 const publicStylesheet = 'public/stylesheets/application.css'
 const backupAppStylesheet = path.join(Cypress.env('tempFolder'), 'temp-application.scss')
 
+const RED = 'rgb(255, 0, 0)'
+const BLACK = 'rgb(11, 12, 12)'
+
 describe('watch sass files', () => {
   describe(`sass file ${cypressTestStylePattern} should be created and included within the ${appStylesheet} and accessible from the browser as /${publicStylesheet}`, () => {
     const cssStatement = `
@@ -27,13 +30,13 @@ describe('watch sass files', () => {
 
       cy.task('deleteFile', { filename: cypressTestStylePattern })
 
-      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', 'rgb(11, 12, 12)')
+      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', BLACK)
       cy.task('deleteFile', { filename: backupAppStylesheet })
     })
 
-    it('The colour of the header should be change to red then back to black', () => {
+    it('The colour of the header should be changed to red then back to black', () => {
       cy.task('log', 'The colour of the header should be black')
-      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', 'rgb(11, 12, 12)')
+      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', BLACK)
 
       cy.task('log', `Create ${cypressTestStylePattern}`)
       cy.task('createFile', {
@@ -50,7 +53,7 @@ describe('watch sass files', () => {
       })
 
       cy.task('log', 'The colour of the header should be changed to red')
-      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', 'rgb(255, 0, 0)')
+      cy.get('.govuk-header', { timeout: 20000 }).should('have.css', 'background-color', RED)
     })
   })
 })
