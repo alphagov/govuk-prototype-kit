976a3e1284baa965b8a4425520acf39c2edd32f5
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-06-24 13:49:13 +0100

    Try and make the watch images integration test less flaky
    The watch-images Cypress test has been very flaky in CI, especially
    recently. This commit tries to make it more reliable by using the
    Cypress `wait()` feature [[1]]. By waiting for Browsersync to reload the
    page after the image has been copied, we can be sure of when the image
    has been copied to the public folder.
    
    Note that Browsersync doesn't put its reloading script on our 404 pages
    or on images, so this commit has to add a page fixture for displaying
    the image.
    
    [1]: https://docs.cypress.io/api/commands/wait

--- cypress/integration/1-watch-files/watch-images.cypress.js
+++ cypress/integration/1-watch-files/watch-images.cypress.js
@@ -7,16 +7,22 @@ const cypressImages = path.join(Cypress.config('fixturesFolder'), 'images')
 const appImages = path.join(Cypress.env('projectFolder'), 'app', 'assets', 'images')
 const publicImages = 'public/images'
 
+const pageFixture = 'larry-the-cat'
+const pageFixtureName = `${pageFixture}.html`
+const pageFixturePath = path.join(Cypress.config('fixturesFolder'), pageFixtureName)
+const pageAppPath = path.join(Cypress.env('projectFolder'), 'app', 'views', pageFixtureName)
+
 describe('watch image files', () => {
   before(() => {
     waitForApplication()
   })
 
   afterEach(() => {
-    // delete temporary files
-    cy.task('deleteFile', { filename: path.join(Cypress.env('projectFolder'), publicImages, imageFile) })
-    cy.wait(2000)
+    // delete test files
     cy.task('deleteFile', { filename: path.join(appImages, imageFile) })
+    cy.task('deleteFile', { filename: path.join(Cypress.env('projectFolder'), publicImages, imageFile) })
+    cy.task('deleteFile', { filename: pageAppPath })
+    cy.wait(2000)
   })
 
   it(`image created in ${appImages} should be copied to ${publicImages} and accessible from the browser`, () => {
@@ -24,8 +30,35 @@ describe('watch image files', () => {
     const target = path.join(appImages, imageFile)
     const publicImage = `${publicImages}/${imageFile}`
 
-    cy.task('log', `Copying ${source} to ${target}`)
+    cy.task('log', 'Creating a page to view our image')
+    cy.task('copyFile', { source: pageFixturePath, target: pageAppPath })
+
+    cy.task('log', 'Our page should be missing its image')
+    cy.visit(`/${pageFixture}`)
+    cy.get('img#larry')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.equal(0)
+      })
+
+    // When we do the copy, we expect the page to reload, so let's set up an
+    // intercept so we can wait for that request
+    cy.intercept(`/${pageFixture}`).as('reloadPage')
+
+    cy.task('log', 'Copying the image to the app folder')
     cy.task('copyFile', { source, target })
+
+    cy.task('log', 'Wait for the page to reload')
+    cy.wait('@reloadPage')
+
+    cy.task('log', 'Our page should now have its image')
+    cy.get('img#larry')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).not.to.equal(0)
+      })
 
     cy.task('log', `Requesting ${publicImage}`)
     cy.request(`/${publicImage}`, { retryOnStatusCodeFailure: true })
