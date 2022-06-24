fe508fbdc96ac1baf03b85114d7f12f114b0ed8e
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-06-24 16:02:39 +0100

    Try and make the watch custom styles integration test less flaky

--- cypress/integration/1-watch-files/watch-custom-styles.cypress.js
+++ cypress/integration/1-watch-files/watch-custom-styles.cypress.js
@@ -2,59 +2,59 @@ const path = require('path')
 
 const { waitForApplication } = require('../utils')
 
-const customStyleFile = path.join(Cypress.env('projectFolder'), 'app', 'assets', 'sass', 'custom.scss')
-const appHeadView = path.join(Cypress.env('projectFolder'), 'app', 'views', 'includes', 'head.html')
-const backupHeadView = path.join(Cypress.env('tempFolder'), 'temp-head.html')
-const publicStylesheet = 'public/stylesheets/custom.css'
+const customStylesFixture = 'custom-styles'
+const customStylesFixtureName = `${customStylesFixture}.scss`
+const customStylesFixturePath = path.join(Cypress.config('fixturesFolder'), 'sass', customStylesFixtureName)
+const customStylesAppPath = path.join(Cypress.env('projectFolder'), 'app', 'assets', 'sass', customStylesFixtureName)
+const customStylesPublicPath = 'public/stylesheets/custom-styles.css'
 
-const GREEN = 'rgb(0, 255, 0)'
-const BLACK = 'rgb(11, 12, 12)'
+const pageFixture = 'custom-styles'
+const pageFixtureName = `${pageFixture}.html`
+const pageFixturePath = path.join(Cypress.config('fixturesFolder'), pageFixtureName)
+const pageAppPath = path.join(Cypress.env('projectFolder'), 'app', 'views', pageFixtureName)
 
 describe('watch custom sass files', () => {
-  describe(`sass file ${customStyleFile} should be created and linked within ${appHeadView} and accessible from the browser as /${publicStylesheet}`, () => {
-    const cssStatement = `
-    .govuk-header { background: ${GREEN}; }
-    `
-
-    before(() => {
+  describe(`sass file ${customStylesFixtureName} should be created and linked within ${pageFixturePath} and accessible from the browser as /${customStylesPublicPath}`, () => {
+    beforeEach(() => {
       waitForApplication()
-      // backup head view
-      cy.task('copyFile', { source: appHeadView, target: backupHeadView })
     })
 
     afterEach(() => {
-      // restore head view
-      cy.task('copyFile', { source: backupHeadView, target: appHeadView })
-
-      // delete temporary files
-      cy.task('deleteFile', { filename: path.join(Cypress.env('projectFolder'), publicStylesheet) })
-      cy.task('deleteFile', { filename: customStyleFile })
-      cy.get('.govuk-header').should('have.css', 'background-color', BLACK)
-      cy.task('deleteFile', { filename: backupHeadView })
+      // delete test files
+      cy.task('deleteFile', { filename: path.join(Cypress.env('projectFolder'), customStylesPublicPath) })
+      cy.task('deleteFile', { filename: customStylesAppPath })
+      cy.task('deleteFile', { filename: pageAppPath })
     })
 
-    it('The colour of the header should be changed to green then back to black', () => {
-      cy.task('log', 'The colour of the header should be black')
-      cy.get('.govuk-header').should('have.css', 'background-color', BLACK)
-
-      cy.task('log', `Create ${customStyleFile}`)
-      cy.task('createFile', {
-        filename: customStyleFile,
-        data: cssStatement
+    it('The colour of the paragraph should be changed to green', () => {
+      cy.task('log', 'Create a page to view our custom styles')
+      cy.task('copyFile', {
+        source: pageFixturePath,
+        target: pageAppPath
       })
 
-      cy.wait(6000)
+      cy.task('log', 'The colour of the paragraph should be black')
+      cy.visit(`/${pageFixture}`)
+      cy.get('p.app-custom-style').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
 
-      cy.task('log', `Amend ${appHeadView} to link ${publicStylesheet}`)
-      cy.task('appendFile', {
-        filename: appHeadView,
-        data: `
-  <link href="/${publicStylesheet}" rel="stylesheet" type="text/css" />
-      `
+      cy.task('log', `Create ${customStylesAppPath}`)
+      cy.task('copyFile', {
+        source: customStylesFixturePath,
+        target: customStylesAppPath
       })
 
-      cy.task('log', 'The colour of the header should be changed to green')
-      cy.get('.govuk-header').should('have.css', 'background-color', GREEN)
+      // When we add our stylesheet, we expect Browsersync to detect that and
+      // tell the browser to fetch it, so let's wait for that resource to
+      // become available.
+      cy.task('log', 'Wait for the stylesheet to be loaded')
+      cy.waitForResource(`${customStylesFixture}.css`)
+
+      cy.task('log', 'The colour of the paragraph should be changed to green')
+      cy.get('p.app-custom-style').should('have.css', 'background-color', 'rgb(0, 255, 0)')
+
+      cy.task('log', `Request ${customStylesPublicPath}`)
+      cy.request(`/${customStylesPublicPath}`)
+        .then(response => expect(response.status).to.eq(200))
     })
   })
 })
