36c925c08330c043492db2a2f41282616aca1430
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2022-06-28 17:03:26 +0100

    Remove dependence on jquery from application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,18 @@
-/* global $ */
+function ready (fn) {
+  if (document.readyState !== 'loading') {
+    // IE9 support
+    fn()
+  } else {
+    // Everything else
+    document.addEventListener('DOMContentLoaded', fn)
+  }
+}
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
-$(document).ready(function () {
+ready(() => {
   window.GOVUKFrontend.initAll()
 })
