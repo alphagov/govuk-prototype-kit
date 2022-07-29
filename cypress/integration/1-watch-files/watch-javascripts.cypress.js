7aeffc5886ceee43427bcc316f2a242dda39a269
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-07-29 10:35:33 +0100

    Add integration test for changing application.js

---
+++ cypress/integration/1-watch-files/watch-javascripts.cypress.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+const { waitForApplication } = require('../utils')
+
+const appJs = path.join(Cypress.env('projectFolder'), 'app', 'assets', 'javascripts', 'application.js')
+const backupAppJs = path.join(Cypress.env('tempFolder'), 'temp-application.js')
+
+describe('watch application.js', () => {
+  before(() => {
+    waitForApplication()
+
+    // backup application.js
+    cy.task('copyFile', { source: appJs, target: backupAppJs })
+  })
+
+  after(() => {
+    // restore files
+    cy.task('copyFile', { source: backupAppJs, target: appJs })
+  })
+
+  it('changes to application.js should be reflected in browser', (done) => {
+    const onAlert = cy.stub()
+    cy.on('window:alert', onAlert)
+
+    const markerText = 'window.GOVUKFrontend.initAll()'
+    const newText = markerText + '\n  ' + "window.alert('Test')"
+
+    cy.task('replaceTextInFile', {
+      filename: appJs,
+      originalText: markerText,
+      newText
+    })
+
+    // wait for page to be reloaded by Browsersync
+    cy.once('window:load', () => {
+      expect(onAlert).to.be.calledWith('Test')
+      done()
+    })
+  })
+})
