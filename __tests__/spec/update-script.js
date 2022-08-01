6aee99117c1b519f210c15b56479635970985f5f
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-08-01 14:41:12 +0100

    Increase test timeout
    The update script tests are quite involved, and on CI they have started
    to take longer than 30 seconds, causing a timeout. Let's increase the
    timeout to 1 minute for these tests.

--- __tests__/spec/update-script.js
+++ __tests__/spec/update-script.js
@@ -13,6 +13,9 @@ const fse = require('fs-extra')
 const execPromise = promisify(child_process.exec)
 const execFilePromise = promisify(child_process.execFile)
 
+// This is a long-running test
+jest.setTimeout(60000)
+
 /*
  * Constants
  */
