f28591145761baef46d2025fb0e34039865f7c8e
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-08-15 14:51:52 +0100

    Remove ability to create release archives of previous releases
    With everything moving around and changing it's difficult to get the
    create-release-archive script to support creating archives of previous
    releases. Luckily, as we removed the update script and its tests in
    commit 958d644, we no longer need that functionality, so let's delete
    the code.
    
    In future if we need to get previous release archives whal I'll suggest
    instead is downlading them from GitHub (with a local cache).

--- __tests__/util/globalSetup.js
+++ __tests__/util/globalSetup.js
@@ -1,9 +1,7 @@
-const { getWorktreeCommit, mkReleaseArchive } = require('./index')
+const { mkReleaseArchive } = require('./index')
 
 module.exports = async function () {
   process.stdout.write('\nDoing global setup...')
-  const worktreeCommit = getWorktreeCommit()
-  process.env.KIT_JEST_WORKTREE_COMMIT = worktreeCommit
   process.env.KIT_JEST_RUN_ID = process.ppid
   const releaseArchive = await mkReleaseArchive()
   process.stdout.write('done\n')
