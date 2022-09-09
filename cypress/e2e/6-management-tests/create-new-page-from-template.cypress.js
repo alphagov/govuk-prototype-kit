a26a1278960944bc57180fa685724a78e627b564
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2022-09-09 11:51:27 +0100

    Management pages acceptance tests

---
+++ cypress/e2e/6-management-tests/create-new-page-from-template.cypress.js
@@ -0,0 +1,81 @@
+const path = require('path')
+
+const { waitForApplication } = require('../utils')
+const { urlencode } = require('nunjucks/src/filters')
+
+const startPageView = path.join(Cypress.env('projectFolder'), 'app', 'views', 'start.html')
+const startPagePath = '/start'
+
+const manageTemplatesPagePath = '/manage-prototype/templates'
+
+const getTemplateLink = (type, packageName, path) => {
+  const queryString = `?package=${urlencode(packageName)}&template=${urlencode(path)}`
+  return `/manage-prototype/templates/${type}${queryString}`
+}
+
+describe('create new start page', () => {
+  before(() => {
+    waitForApplication()
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // we expect an error with message 'Cannot read properties of undefined (reading 'documentReady')'
+      // and don't want to fail the test so we return false
+      if (err.message.includes(`Cannot read properties of undefined (reading 'documentReady')`)) {
+        return false
+      }
+      // we still want to ensure there are no other unexpected
+      // errors, so we let them fail the test
+    })
+    cy.task('deleteFile', { filename: startPageView })
+    waitForApplication()
+  })
+
+  after(() => {
+    cy.task('deleteFile', { filename: startPageView })
+  })
+
+  it('View the start page from the management page', () => {
+    cy.task('log', 'Visit the manage prototype templates page')
+    cy.visit(manageTemplatesPagePath)
+    cy.get(`a[href="${getTemplateLink('view', 'govuk-prototype-kit', `/lib/templates/start.html`)}"]`).click()
+
+    cy.task('log', 'The start page template should be displayed')
+    cy.get('.govuk-button--start')
+      .should('contains.text', 'Start now')
+  })
+
+  it('Create the start page from the management page', () => {
+    cy.task('log', 'The start page should not be found')
+    cy.visit(startPagePath, { failOnStatusCode: false })
+    cy.get('body')
+      .should('contains.text', `Page not found: ${startPagePath}`)
+
+    cy.task('log', 'Visit the manage prototype templates page')
+    cy.visit(manageTemplatesPagePath)
+
+    cy.get(`a[href="${getTemplateLink('install', 'govuk-prototype-kit', `/lib/templates/start.html`)}"]`).click()
+
+    cy.task('log', 'Create the page')
+    cy.get('.govuk-heading-l')
+      .should('contains.text', 'Create new page from template')
+    cy.get('.govuk-body')
+      .should('contains.text', 'Using template "Start page".')
+    cy.get('#chosen-url')
+      .type(startPagePath)
+    cy.get('.govuk-button')
+      .should('contains.text', 'Create page').click()
+
+    cy.task('log', 'Confirmation page')
+    cy.get('.govuk-heading-l')
+      .should('contains.text', 'Page created')
+
+    cy.task('log', 'Confirm the page exists')
+    cy.get(`a[href="${startPagePath}"]`)
+      .should('contains.text', startPagePath).click()
+    cy.task('log', 'The start page should be displayed')
+    cy.get('.govuk-button--start')
+      .should('contains.text', 'Start now')
+
+    cy.task('log', 'Confirm the view of the page exists where expected')
+    cy.task('existsFile', { filename: startPageView })
+  })
+})
