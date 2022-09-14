683dd8a21abaf2aa124b52310686264a6a9c62f1
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2022-09-14 15:27:25 +0100

    Test installing plugins

--- cypress/e2e/6-management-tests/install-available-plugin.cypress.js
+++ cypress/e2e/6-management-tests/install-available-plugin.cypress.js
@@ -1,7 +1,10 @@
 const { waitForApplication } = require('../utils')
+const path = require('path')
 const managePluginsPagePath = '/manage-prototype/plugins'
 const plugin = 'hmrc-frontend'
 const pluginName = 'HMRC Frontend'
+
+const pluginPackageJson = path.join(Cypress.env('projectFolder'), 'node_modules', plugin, 'package.json')
 
 describe('install available plugin', () => {
   before(() => {
@@ -23,5 +26,42 @@ describe('install available plugin', () => {
 
     cy.get('code')
       .should('have.text', `npm install ${plugin}`)
+
+    cy.exec(`cd ${Cypress.env('projectFolder')} && npm install ${plugin}`)
+
+    cy.task('existsFile', { filename: pluginPackageJson, timeout: 15000 })
+
+    cy.wait(5000)
+
+    waitForApplication(managePluginsPagePath)
+
+    cy.get('h2.govuk-heading-m', { timeout: 20000 }).eq(0)
+      .should('contains.text', 'Installed')
+
+    cy.task('log', `Uninstall the ${plugin} plugin`)
+    cy.get(`a[href*="/uninstall?package=${plugin}"]`)
+      .should('contains.text', 'Uninstall').click()
+
+    cy.task('log', `The ${plugin} plugin should be displayed`)
+    cy.get('h1')
+      .should('contains.text', `Uninstall ${pluginName}`)
+
+    cy.get('code')
+      .should('have.text', `npm uninstall ${plugin}`)
+
+    cy.exec(`cd ${Cypress.env('projectFolder')} && npm uninstall ${plugin}`)
+
+    cy.task('notExistsFile', { filename: pluginPackageJson, timeout: 15000 })
+
+    cy.wait(5000)
+
+    waitForApplication(managePluginsPagePath)
+
+    cy.get('h2.govuk-heading-m', { timeout: 20000 }).eq(1)
+      .should('contains.text', 'Available')
+
+    cy.task('log', `Install the ${plugin} plugin`)
+    cy.get(`a[href*="/install?package=${plugin}"]`)
+      .should('contains.text', 'Install')
   })
 })
