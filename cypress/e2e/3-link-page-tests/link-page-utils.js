3eab1c44dcfc76c9c7f47b3bdf6f670f8fad294b
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2022-09-22 13:35:07 +0100

    Not creating any files when everything is missing.

--- cypress/e2e/3-link-page-tests/link-page-utils.js
+++ cypress/e2e/3-link-page-tests/link-page-utils.js
@@ -79,11 +79,11 @@ const setUpPages = () => {
 }
 
 const setUpData = () => {
-  cy.task('replaceTextInFile', { filename: appDataFile, originalText: '// Insert values here', newText: `"how-many-balls": "${defaultHowManyBalls}", "most-impressive-trick": "${defaultMostImpressiveTrick}"` })
+  cy.task('createFile', { filename: appDataFile, data: `module.exports = {"how-many-balls": "${defaultHowManyBalls}", "most-impressive-trick": "${defaultMostImpressiveTrick}"}` })
 }
 
 const clearUpData = () => {
-  cy.task('replaceTextInFile', { filename: appDataFile, originalText: `"how-many-balls": "${defaultHowManyBalls}", "most-impressive-trick": "${defaultMostImpressiveTrick}"`, newText: '// Insert values here' })
+  cy.task('deleteFile', { filename: appDataFile })
 }
 
 const setUpBranchingPages = () => {
