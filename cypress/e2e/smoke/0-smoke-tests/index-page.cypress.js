aabfe57901c9eb36392176980db9bb0c5ce9f61e
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2022-11-02 11:13:55 +0000

    Fix loading of front-end legacy assets

--- cypress/e2e/smoke/0-smoke-tests/index-page.cypress.js
+++ cypress/e2e/smoke/0-smoke-tests/index-page.cypress.js
@@ -2,3 +2,13 @@ specify('index page', () => {
   cy.visit('/')
   cy.contains('GOV.UK Prototype Kit')
 })
+
+specify('govuk-frontend fonts loaded', () => {
+  cy.visit('/')
+
+  const fontUrl = '/extension-assets/govuk-frontend/govuk/assets/fonts/bold-b542beb274-v2.woff2'
+
+  cy.task('log', `Requesting govuk-frontend font`)
+  cy.request(`/${fontUrl}`, { retryOnStatusCodeFailure: true })
+    .then(response => expect(response.status).to.eq(200))
+})
