e3c47cd080929f437670447897b3b8d5f8cd4f40
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-01-04 15:26:10 +0000

    Move call of listenOnAvailablePort to dev-server code
    This fixes #462, as the `process.exit(0)` is no longer done within the
    `nodemon` process. It also means we're not running portScanner every time
    the kit restarts, and we can remove the code saving the port number to a
    file.
    
    Note changing the port number in `app/config.json` will now require a
    manual restart.

--- listen-on-port.js
+++ listen-on-port.js
@@ -5,36 +5,35 @@ const browserSync = require('browser-sync')
 // local dependencies
 const server = require('./server.js')
 const config = require('./lib/config.js').getConfig()
-const utils = require('./lib/utils')
+
+const port = config.port
 
 if (config.isTest) {
   server.listen()
 } else {
-  utils.findAvailablePort((port) => {
-    if (config.isDevelopment) {
-      console.log('You can manage your prototype at:')
-      console.log(`http://localhost:${port}/manage-prototype`)
-      console.log('')
-    }
-    console.log('The Prototype Kit is now running at:')
-    console.log(`http://localhost:${port}`)
+  if (config.isDevelopment) {
+    console.log('You can manage your prototype at:')
+    console.log(`http://localhost:${port}/manage-prototype`)
     console.log('')
+  }
+  console.log('The Prototype Kit is now running at:')
+  console.log(`http://localhost:${port}`)
+  console.log('')
 
-    if (config.isProduction || !config.useBrowserSync) {
-      server.listen(port)
-    } else {
-      server.listen(port - 50, () => {
-        browserSync({
-          proxy: 'localhost:' + (port - 50),
-          port,
-          ui: false,
-          files: ['.tmp/public/**/*.*', 'app/views/**/*.*', 'app/assets/**/*.*', 'app/config.json'],
-          ghostMode: false,
-          open: false,
-          notify: false,
-          logLevel: 'error'
-        })
+  if (config.isProduction || !config.useBrowserSync) {
+    server.listen(port)
+  } else {
+    server.listen(port - 50, () => {
+      browserSync({
+        proxy: 'localhost:' + (port - 50),
+        port,
+        ui: false,
+        files: ['.tmp/public/**/*.*', 'app/views/**/*.*', 'app/assets/**/*.*', 'app/config.json'],
+        ghostMode: false,
+        open: false,
+        notify: false,
+        logLevel: 'error'
       })
-    }
-  })
+    })
+  }
 }
