61bfc9112fce7ed0c8ad57e0df93ff2de135e8c6
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2023-01-09 17:07:37 +0000

    Added auth token for passwordless login.

--- cypress/e2e/prod/2-management-tests/password-page.cypress.js
+++ cypress/e2e/prod/2-management-tests/password-page.cypress.js
@@ -1,11 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+
 const homePath = '/index'
 const passwordPath = '/manage-prototype/password'
 const errorQuery = 'error=wrong-password'
 const returnURLQuery = `returnURL=${encodeURIComponent(homePath)}`
 
 describe('password page', () => {
+  let authToken
   before(() => {
     cy.task('waitUntilAppRestarts')
+    authToken = JSON.parse(fs.readFileSync(path.join(Cypress.env('projectFolder'), 'app', 'config.json'), 'utf8')).authToken
+    console.log('auth token', authToken)
   })
 
   it('valid password', () => {
@@ -29,6 +35,22 @@ describe('password page', () => {
       cy.get('.govuk-error-summary__list').should('contains.text', 'The password is not correct')
       cy.get('#password-error').should('contains.text', 'The password is not correct')
       cy.url().should('eq', `${urlObject.origin + passwordPath}?${errorQuery}&${returnURLQuery}`)
+    })
+  })
+
+  it('valid token', () => {
+    cy.visit(homePath + `?loginToken=${encodeURIComponent(authToken)}`)
+    cy.url().then(homeUrl => {
+      const urlObject = new URL(homeUrl)
+      expect(homeUrl).equal(`${urlObject.origin + homePath}`)
+    })
+  })
+
+  it('invalid token', () => {
+    cy.visit(homePath + `?loginToken=${encodeURIComponent(authToken.substr(0, -1))}`)
+    cy.url().then(passwordUrl => {
+      const urlObject = new URL(passwordUrl)
+      expect(passwordUrl).equal(`${urlObject.origin + passwordPath}?${returnURLQuery}`)
     })
   })
 })
