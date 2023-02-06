b3e150e38a1487e29dd879beb2021b6b2382e88f
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2023-02-06 14:14:51 +0000

    Let cypress test the symptom and not the implementation

--- cypress/e2e/dev/1-watch-files/watch-javascripts.cypress.js
+++ cypress/e2e/dev/1-watch-files/watch-javascripts.cypress.js
@@ -1,4 +1,3 @@
-
 // core dependencies
 const path = require('path')
 
@@ -8,31 +7,34 @@ const { waitForApplication } = require('../../utils')
 const appJsPath = path.join('app', 'assets', 'javascripts', 'application.js')
 const appJs = path.join(Cypress.env('projectFolder'), appJsPath)
 
+const heading = 'Test Heading'
+
+const scriptToAdd = `
+  document.querySelector("h1").innerHTML = "${heading}"
+`
+
 describe('watch application.js', () => {
   before(() => {
     // Restore application.js from prototype starter
     cy.task('copyFromStarterFiles', { filename: appJsPath })
-    waitForApplication()
   })
 
-  it('changes to application.js should be reflected in browser', (done) => {
-    const onAlert = cy.stub()
-    cy.on('window:alert', onAlert)
+  it('changes to application.js should be reflected in browser', () => {
+    waitForApplication()
+
+    cy.get('h1').should('not.contain.text', heading)
 
     const markerText = '// Add JavaScript here'
-    const newText = markerText + '\n  ' + "window.alert('Test')"
 
     cy.task('replaceTextInFile', {
       filename: appJs,
       originalText: markerText,
-      newText
+      newText: markerText + scriptToAdd
     })
 
-    // wait for page to be reloaded by Browsersync
-    cy.once('window:load', () => {
-      cy.wait(1000)
-      expect(onAlert).to.be.calledWith('Test')
-      done()
-    })
+    cy.get('h1').contains(heading)
+
+    // Restore application.js from prototype starter
+    cy.task('copyFromStarterFiles', { filename: appJsPath })
   })
 })
