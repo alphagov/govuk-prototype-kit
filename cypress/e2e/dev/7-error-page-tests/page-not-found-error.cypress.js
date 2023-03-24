fe36e3a78b3d0fbb742f0f24d76bbb0ff17d057b
Ollie Beumkes <oliver.beumkes@digital.cabinet-office.gov.uk>
2023-03-24 13:12:27 +0000

    Added cypress tests to test new 404 error

---
+++ cypress/e2e/dev/7-error-page-tests/page-not-found-error.cypress.js
@@ -0,0 +1,29 @@
+const { forEach } = require('lodash')
+const { waitForApplication } = require('../../utils')
+
+const pageName = 'Page not found'
+const checkPageNotFoundText = 'There is no page at /p4ge-n0t-f0und'
+const helpText = 'This may be because:'
+const helpList = [
+    'you typed or pasted the web address incorrectly',
+    'a link in your code is wrong',
+    'a form in your code is wrong',
+    'you have not created the page yet']
+const contactSupportText = 'You can try and fix this yourself or contact the GOV.UK Prototype Kit team if you need help.'
+
+it('internal server error results in 500 page being displayed correctly', () => {
+    waitForApplication()
+    cy.visit('/p4ge-n0t-f0und', { failOnStatusCode: false })
+  
+    cy.get('.govuk-heading-l').contains(pageName)
+
+    cy.get('.govuk-body').contains(checkPageNotFoundText)
+  
+    cy.get('.govuk-body').contains(helpText)
+
+    helpList.forEach(hint => {
+        cy.get('li').contains(hint)
+    })
+
+    cy.get('.govuk-body').contains(contactSupportText)
+  })
