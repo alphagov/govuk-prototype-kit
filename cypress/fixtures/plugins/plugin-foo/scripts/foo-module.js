d2f8fda4cbeb3a860add57d070aaf876b98b2768
Ben Surgison <ben.surgison@digital.cabinet-office.gov.uk>
2023-04-05 18:52:36 +0100

    Implement type equals module

---
+++ cypress/fixtures/plugins/plugin-foo/scripts/foo-module.js
@@ -0,0 +1,10 @@
+import fooSubmodule from './foo-submodule.js'
+
+const fooParagraph = document.getElementById('foo-module')
+
+if (fooParagraph) {
+  fooParagraph.hidden = false
+  setTimeout(() => {
+    fooParagraph.innerHTML = 'The foo result is: ' + fooSubmodule(1, 2)
+  }, 500)
+}
