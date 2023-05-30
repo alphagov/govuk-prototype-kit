a086337528177437da0b1c60698ddd127f3b4b98
HannahJMWood <124601633+HannahJMWood@users.noreply.github.com>
2023-05-30 14:35:25 +0100

    addingd login for testing

--- __tests__/spec/plugin-validator.js
+++ __tests__/spec/plugin-validator.js
@@ -1,36 +1,41 @@
+/* eslint-env jest */
+
 const childProcess = require('node:child_process')
 
-function run (path) {
-  const stdOutParts = []
-  const stdErrParts = []
-  return new Promise((resolve, reject) => {
-    const cp = childProcess.spawn('./bin/cli', ['validate-plugin', path], {
-      env: {
-        ...process.env
-      }
-    })
+// function run (path) {
+//   const stdOutParts = []
+//   const stdErrParts = []
+//   return new Promise((resolve, reject) => {
+//     const cp = childProcess.spawn('./bin/cli', ['validate-plugin', path], {
+//       env: {
+//         ...process.env
+//       }
+//     })
 
-    cp.stdout.on('data', (data) => {
-      stdOutParts.push(data.toString())
-    })
-    cp.stderr.on('data', (data) => {
-      stdErrParts.push(data.toString())
-    })
-    cp.on('exit', (statusCode) => {
-      if (statusCode === 0) {
-        resolve({
-          stdout: stdOutParts.join('\n'),
-          stderr: stdErrParts.join('\n'),
-          statusCode
-        })
-      }
-    })
-  })
-}
+//     cp.stdout.on('data', (data) => {
+//       stdOutParts.push(data.toString())
+//     })
+//     cp.stderr.on('data', (data) => {
+//       stdErrParts.push(data.toString())
+//     })
+//     cp.on('exit', (statusCode) => {
+//       if (statusCode === 0) {
+//         resolve({
+//           stdout: stdOutParts.join('\n'),
+//           stderr: stdErrParts.join('\n'),
+//           statusCode
+//         })
+//       }
+//     })
+//   })
+// }
+
+const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'test-v11-prototype')
+const cliPath = path.join(__dirname, '..', '..', 'bin', 'cli')
 
 describe('plugin-validator', () => {
   it('should work', async () => {
-    const result = await run('../fixtures/working-plugin')
+    const result = await run('../fixtures/mockPlugins/valid-plugin')
 
     expect(result).toEqual({
       stdout: 'Your plugin is valid.',
@@ -38,13 +43,13 @@ describe('plugin-validator', () => {
       statusCode: 0
     })
   })
-  it('should return list of errors found', async () => {
-    const result = await run('../fixtures/broken-plugin')
+  //   it('should return list of errors found', async () => {
+  //     const result = await run('../fixtures/mockPlugins/invalid-plugin')
 
-    expect(result).toEqual({
-      stdout: '',
-      stderr: 'Error in plugin:\n\n- Missing forwardslash on xyz',
-      statusCode: 0
-    })
-  })
+//     expect(result).toEqual({
+//       stdout: '',
+//       stderr: 'Error in plugin:\n\n- Missing forwardslash on xyz',
+//       statusCode: 0
+//     })
+//   })
 })
