947d566a8dec45c5e28c3df144e32dcc58e51e23
Ollie Beumkes <oliver.beumkes@digital.cabinet-office.gov.uk>
2023-05-31 12:24:12 +0100

    refactored valid and invalid tests

--- __tests__/spec/plugin-validator.js
+++ __tests__/spec/plugin-validator.js
@@ -9,34 +9,28 @@ async function runShellCommand (fixtureProjectDirectory, cb) {
   await exec(`"${process.execPath}" ${cliPath} validate-plugin ${fixtureProjectDirectory}`,
     { cwd: fixtureProjectDirectory, env: process.env, stdio: 'inherit' }, function (err, stdout, stderr) {
       if (err) {
+        console.log('stderr: ' + stdout)
         cb(stderr)
       } else {
-        cb(JSON.stringify(stdout))
+        console.log('stdout: ' + stdout)
+        cb(stdout)
       }
     })
 }
 
 describe('plugin-validator', () => {
-  it('should work', () => {
+  it('should work', async () => {
     const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'valid-plugin')
     runShellCommand(fixtureProjectDirectory, function (result) {
       // handle errors here
-      console.log(result)
+      expect(result[0]).toEqual('The plugin config is valid.')
     })
   })
   it('should return list of errors found', async () => {
     const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'invalid-plugin')
-
-    return run(fixtureProjectDirectory)
-      .then((result) => {
-        expect(result).toEqual({
-          stdout: 'Error in plugin:\n\n- Missing forwardslash on xyz',
-          stderr: '',
-          statusCode: 0
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    runShellCommand(fixtureProjectDirectory, function (result) {
+      // handle errors here
+      expect(result[0]).toEqual('Error in plugin:\n\n- Missing forwardslash on xyz')
+    })
   })
 })
