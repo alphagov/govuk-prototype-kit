795aefbb98b1bc9bbf6a6aa646d20b8701e86dec
Ollie Beumkes <oliver.beumkes@digital.cabinet-office.gov.uk>
2023-05-31 11:28:58 +0100

    refactored testing for plugins validator

--- __tests__/spec/plugin-validator.js
+++ __tests__/spec/plugin-validator.js
@@ -1,51 +1,32 @@
 /* eslint-env jest */
 
-const childProcess = require('node:child_process')
 const path = require('path')
 
-async function run (pathToPlugin) {
-  const stdOutParts = []
-  const stdErrParts = []
-  return new Promise((resolve, reject) => {
-    const cp = childProcess.spawn('./bin/cli', ['validate-plugin', path], {
-      env: {
-        ...process.env
+const cliPath = path.join(__dirname, '..', '..', 'bin', 'cli')
+const { exec } = require('child_process')
+
+async function runShellCommand (fixtureProjectDirectory, cb) {
+  await exec(`"${process.execPath}" ${cliPath} validate-plugin ${fixtureProjectDirectory}`,
+    { cwd: fixtureProjectDirectory, env: process.env, stdio: 'inherit' }, function (err, stdout, stderr) {
+      if (err) {
+        cb(stderr)
+      } else {
+        cb(JSON.stringify(stdout))
       }
     })
-
-    cp.stdout.on('data', (data) => {
-      stdOutParts.push(data.toString())
-    })
-    cp.stderr.on('data', (data) => {
-      stdErrParts.push(data.toString())
-    })
-    cp.on('exit', (statusCode) => resolve({
-        stdout: stdOutParts.join('\n'),
-        stderr: stdErrParts.join('\n'),
-        statusCode
-      })
-    )
-  })
 }
 
 describe('plugin-validator', () => {
   it('should work', () => {
     const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'valid-plugin')
-    return run(fixtureProjectDirectory)
-      .then((result) => {
-        expect(result).toEqual({
-          stdout: 'Your plugin is valid.',
-          stderr: '',
-          statusCode: 0
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    runShellCommand(fixtureProjectDirectory, function (result) {
+      // handle errors here
+      console.log(result)
+    })
   })
   it('should return list of errors found', async () => {
     const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'invalid-plugin')
-    
+
     return run(fixtureProjectDirectory)
       .then((result) => {
         expect(result).toEqual({
