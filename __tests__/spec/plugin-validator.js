621b7e1662ee0b2b4f766c0e84c334e4de7d5381
HannahJMWood <124601633+HannahJMWood@users.noreply.github.com>
2023-06-02 11:04:47 +0100

    adding error message to test for invalid plugin

--- __tests__/spec/plugin-validator.js
+++ __tests__/spec/plugin-validator.js
@@ -29,13 +29,14 @@ describe('plugin-validator', () => {
   })
 
   it('should return list of errors found', async () => {
+    const expectedOutput = 'The following invalid keys exist in your config: scss,In section scss, the path "/sass/_step-by-step-navigation.scss" does not exist,In section scss, the path "/sass/_step-by-step-navigation-header.scss" does not exist,In section scss, the path "/sass/_step-by-step-navigation-related.scss" does not exist,In section scripts, the path "javascripts/step-by-step-navigation.js" does not start with a ' / ',In section scripts, the path "javascripts/step-by-step-polyfills.js" does not start with a ' / ',In section templates, the path "/templates/step-by-step-navigation.html" does not exist,In section templates, the path "/templates/start-with-step-by-step.html" does not exist'
     const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'invalid-plugin')
 
     runShellCommand(fixtureProjectDirectory, function cb (result) {
       const outputs = result.split('\n')
       const outputToCheck = outputs[outputs.length - 2]
 
-      expect(outputToCheck).toEqual('The plugin config is valid.')
+      expect(outputToCheck).toEqual(expectedOutput)
     }).then(res => console.log('result: ' + res)
     )
   })
