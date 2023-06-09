1ea92dc2534aabe86b81f9da8df0644ab0e4c3ab
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2023-06-09 12:55:22 +0100

    Accepting path on command line, seperating stdout and stderr, providing exit code.

--- __tests__/spec/plugin-validator.js
+++ __tests__/spec/plugin-validator.js
@@ -2,77 +2,72 @@
 
 const path = require('path')
 const cliPath = path.join(__dirname, '..', '..', 'bin', 'cli')
-const { exec } = require('child_process')
+const {exec} = require('child_process')
 
-function runShellCommand (fixtureProjectDirectory) {
+function runShellCommand(fixtureDirectoryName) {
+  const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins',fixtureDirectoryName) 
   return new Promise((resolve, reject) => {
-    exec(`"${process.execPath}" ${cliPath} validate-plugin ${fixtureProjectDirectory}`,
-      { cwd: fixtureProjectDirectory, env: process.env, stdio: 'inherit' }, function (err, stdout, stderr) {
-        if (err) {
-          reject(stderr)
-        } else {
-          resolve(stdout)
-        }
+    const abc = exec(`"${process.execPath}" ${cliPath} validate-plugin ${fixtureProjectDirectory}`,
+      {env: process.env, stdio: 'inherit'}, function (err, stdout, stderr) {
+        resolve({
+          stdout,
+          stderr,
+          exitCode: abc.exitCode
+        })
       })
   })
 }
 
 describe('plugin-validator', () => {
   it('should work', async () => {
-    const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'valid-plugin')
-    const result = await runShellCommand(fixtureProjectDirectory)
-    const outputs = result.split('\n')
-    const outputToCheck = outputs[outputs.length - 2]
+    const result = await runShellCommand('valid-plugin')
 
-    expect(outputToCheck).toEqual('The plugin config is valid.')
+    expect(result.stdout).toEqual(`Config file exists, validating contents.
+Validating whether config paths meet criteria.
+The plugin config is valid.
+`)
   })
 
   it('should return list of path errors found', async () => {
-    const expectedOutput = 'In section sass, the path \'/sass/_step-by-step-navigation.scss\' does not exist,In section sass, the path \'/sass/_step-by-step-navigation-header.scss\' does not exist,In section sass, the path \'/sass/_step-by-step-navigation-related.scss\' does not exist,In section scripts, the path \'javascripts/step-by-step-navigation.js\' does not start with a \'/\',In section scripts, the path \'javascripts/step-by-step-polyfills.js\' does not start with a \'/\',In section scripts, the path \'javascripts/modules/foo-module-one.js\' does not start with a \'/\',In section templates, the path \'/templates/step-by-step-navigation.html\' does not exist,In section templates, the path \'/templates/start-with-step-by-step.html\' does not exist'
-    const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'invalid-plugin')
+    const result = await runShellCommand('invalid-plugin')
 
-    const result = await runShellCommand(fixtureProjectDirectory)
-    const outputs = result.split('\n')
-    const outputToCheck = outputs[outputs.length - 2]
-
-    expect(outputToCheck).toEqual(expectedOutput)
+    expect(result.exitCode).toEqual(100)
+    expect(result.stderr).toEqual(`Error: In section sass, the path '/sass/_step-by-step-navigation.scss' does not exist
+Error: In section sass, the path '/sass/_step-by-step-navigation-header.scss' does not exist
+Error: In section sass, the path '/sass/_step-by-step-navigation-related.scss' does not exist
+Error: In section scripts, the path 'javascripts/step-by-step-navigation.js' does not start with a '/'
+Error: In section scripts, the path 'javascripts/step-by-step-polyfills.js' does not start with a '/'
+Error: In section scripts, the path 'javascripts/modules/foo-module-one.js' does not start with a '/'
+Error: In section templates, the path '/templates/step-by-step-navigation.html' does not exist
+Error: In section templates, the path '/templates/start-with-step-by-step.html' does not exist
+`)
   })
 
   it('should return list of invalid keys', async () => {
-    const expectedOutput = 'The following invalid keys exist in your config: scss,unknown-key'
-    const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'plugin-invalid-keys')
+    const result = await runShellCommand('plugin-invalid-keys')
 
-    const result = await runShellCommand(fixtureProjectDirectory)
-    const outputs = result.split('\n')
-    const outputToCheck = outputs[outputs.length - 2]
-
-    expect(outputToCheck).toEqual(expectedOutput)
+    expect(result.exitCode).toEqual(100)
+    expect(result.stderr).toEqual('Error: The following invalid keys exist in your config: scss,unknown-key\n')
   })
 
   it('should return error because config does not exist', async () => {
-    const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'plugin-no-config')
-    const result = await runShellCommand(fixtureProjectDirectory)
-    const outputs = result.split('\n')
-    const outputToCheck = outputs[outputs.length - 2]
+    const result = await runShellCommand('plugin-no-config')
 
-    expect(outputToCheck).toEqual('The plugin does not have a govuk-prototype-kit.config.json file, all plugins must have this file to be valid.')
+    expect(result.exitCode).toEqual(100)
+    expect(result.stderr).toEqual('Error: The plugin does not have a govuk-prototype-kit.config.json file, all plugins must have this file to be valid.\n')
   })
 
   it('should return error because config is not a valid json', async () => {
-    const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'plugin-invalid-json')
-    const result = await runShellCommand(fixtureProjectDirectory)
-    const outputs = result.split('\n')
-    const outputToCheck = outputs[outputs.length - 2]
+    const result = await runShellCommand('plugin-invalid-json')
 
-    expect(outputToCheck).toEqual('Your govuk-prototype-kit.config.json file is not valid json.')
+    expect(result.exitCode).toEqual(100)
+    expect(result.stderr).toEqual('Error: Your govuk-prototype-kit.config.json file is not valid json.\n')
   })
 
   it('should return error because config is empty', async () => {
-    const fixtureProjectDirectory = path.join(__dirname, '..', 'fixtures', 'mockPlugins', 'plugin-empty-config')
-    const result = await runShellCommand(fixtureProjectDirectory)
-    const outputs = result.split('\n')
-    const outputToCheck = outputs[outputs.length - 2]
+    const result = await runShellCommand('plugin-empty-config')
 
-    expect(outputToCheck).toEqual('There are no contents in your govuk-prototype.config file!')
+    expect(result.exitCode).toEqual(100)
+    expect(result.stderr).toEqual('Error: There are no contents in your govuk-prototype.config file!\n')
   })
 })
