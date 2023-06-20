26e92fd344830215f87ecb40d3cedffbf265eaec
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2023-06-20 10:29:44 +0100

    Added cache for config lookup.

--- proof-of-concept.js
+++ proof-of-concept.js
@@ -1,44 +1,10 @@
-const {requestHttpsJson, requestHttps, findFileInHttpsTgz} = require("./lib/utils/requestHttps")
-const {startPerformanceTimer, endPerformanceTimer} = require("./lib/utils/performance");
-
-async function getConfigForPackage(packageName) {
-  const timer = startPerformanceTimer()
-
-  const registry = await requestHttpsJson(`https://registry.npmjs.org/${encodeURIComponent(packageName)}`)
-  const latestTag = registry['dist-tags']?.latest
-
-  if (!latestTag) {
-    return
-  }
-
-  const url = registry.versions[latestTag].dist.tarball
-  const result = await findFileInHttpsTgz(url, {
-    fileToFind: 'package/govuk-prototype-kit.config.json',
-    prepare: str => {
-      if (str && str.startsWith('{')) {
-        return JSON.parse(str)
-      }
-    }
-  })
-
-  endPerformanceTimer('getConfigForPackage', timer)
-
-  return result
-}
+const {startPerformanceTimer, endPerformanceTimer} = require("./lib/utils/performance")
+const {getConfigForPackage} = require("./lib/utils/requestHttps");
 
 async function demo(packageName) {
-  console.log('')
-  console.log('')
+  await getConfigForPackage(packageName);
 
-  const result = await getConfigForPackage(packageName);
-
-  console.log('')
-  console.log('')
-
-  console.log(result)
-
-  console.log('')
-  console.log('')
+  console.log('got result for ', packageName)
 }
 
 (async () => {
@@ -47,6 +13,7 @@ async function demo(packageName) {
   await demo('hmrc-frontend')
   await demo('govuk-frontend')
   await demo('@x-govuk/edit-prototype-in-browser')
-  // await demo('nhsuk-frontend')
+  await demo('nhsuk-frontend')
+  await demo('lkasdjflkajsdflkfjsad')
   endPerformanceTimer('Demo', timer)
 })()
