066e60ce0539c316ab84f798bf15c1f233637aad
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2023-07-14 14:05:06 +0100

    Allowing upgrade and update to be used interchangably in the URLs (to support updating the kit).

---
+++ cypress/e2e/plugins/2-prototype-kit-plugin-tests/allow-upgrade-in-url.cypress.js
@@ -0,0 +1,34 @@
+const { replaceInFile, waitForApplication, uninstallPlugin, log } = require('../../utils')
+const path = require('path')
+const { performPluginAction } = require('../plugin-utils')
+const plugin = '@govuk-prototype-kit/task-list'
+const pluginVersion = '1.1.1'
+const originalText = '"dependencies": {'
+const replacementText = `"dependencies": { "${plugin}": "${pluginVersion}",`
+const pkgJsonFile = path.join(Cypress.env('projectFolder'), 'package.json')
+
+function restore () {
+  uninstallPlugin(plugin)
+}
+
+describe('Allow upgrade in URLs', () => {
+  before(restore)
+  after(restore)
+
+  it('When updating the old upgrade URL should still work', () => {
+    waitForApplication()
+
+    log(`Add an old version of ${plugin} within the package.json`)
+    replaceInFile(pkgJsonFile, originalText, '', replacementText)
+    cy.exec(`cd ${Cypress.env('projectFolder')} && npm install`)
+
+    log('Make sure old upgrade URL still works')
+    cy.visit(`/manage-prototype/plugins/upgrade?package=${encodeURIComponent(plugin)}`)
+    cy.get('button#plugin-action-button')
+      .contains('Update')
+      .click()
+
+    log('Force the plugins to be installed with an npm install')
+    performPluginAction('update', plugin, 'Task List')
+  })
+})
