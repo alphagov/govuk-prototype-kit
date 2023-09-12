564f5abda639105d21eaca197467238a7fb8aaa8
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2023-09-12 10:34:54 +0100

    Trying to use npx instead of cross-spawn (before testing with cross-spawn)

--- features/support/global/initKit.js
+++ features/support/global/initKit.js
@@ -1,5 +1,4 @@
 const cp = require('child_process')
-const { spawn: crossSpawn } = require('cross-spawn')
 const path = require('path')
 const events = require('events')
 
@@ -52,7 +51,7 @@ function initKit (config) {
 
   return new Promise((resolve, reject) => {
     let startCommand
-    const initProcess = crossSpawn('./bin/cli', ['create', `--version=${rootDir}`, tmpDir], { cwd: rootDir })
+    const initProcess = cp.spawn('npx', ['govuk-prototype-kit@./bin/cli', 'create', `--version=${rootDir}`, tmpDir], { cwd: rootDir })
     initProcess.stderr.on('data', (data) => console.warn('[stderr]', data.toString()))
     initProcess.stdout.on('data', (data) => {
       const str = data.toString()
