9daad50a7d98f9de3e95bde8fa7726f4c7510382
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-01-30 16:14:11 +0000

    Make server startup wait for package cache
    As the server startup compiles the assets and
    Sass functions need the package cache to be
    populated, add a `waitForPackageCache` before
    assets are generated.

--- listen-on-port.js
+++ listen-on-port.js
@@ -1,49 +1,55 @@
 // npm dependencies
 const { runErrorServer } = require('./lib/errorServer')
+const { waitForPackagesCache } = require('./lib/plugins/packages.js')
 const { verboseLog } = require('./lib/utils/verboseLogger')
 
 const config = require('./lib/config.js').getConfig(null, false)
 
-try {
-  // local dependencies
-  const syncChanges = require('./lib/sync-changes')
-  const server = require('./server.js')
-  const { generateAssetsSync } = require('./lib/build')
+;(async () => {
+  try {
+    // local dependencies
+    const syncChanges = require('./lib/sync-changes')
+    const server = require('./server.js')
+    const { generateAssetsSync } = require('./lib/build')
 
-  const port = config.port
-  const proxyPort = port - 50
+    const port = config.port
+    const proxyPort = port - 50
 
-  generateAssetsSync()
+    // Wait for the package cache to be built before doing anything
+    // to ensure that `pluginVersionSatisfies` runs against accurate data
+    await waitForPackagesCache()
+    generateAssetsSync()
 
-  if (config.isTest) {
-    server.listen()
-  } else {
-    if (config.isDevelopment) {
-      console.log('You can manage your prototype at:')
-      console.log(`http://localhost:${port}/manage-prototype`)
-      console.log('')
-    }
-    console.log('The Prototype Kit is now running at:')
-    console.log(`http://localhost:${port}`)
-    console.log('')
-
-    if (config.isProduction || !config.useBrowserSync) {
-      server.listen(port)
+    if (config.isTest) {
+      server.listen()
     } else {
-      server.listen(proxyPort, () => {
-        syncChanges.sync({
-          port,
-          proxyPort,
-          files: ['.tmp/public/**/*.*', 'app/views/**/*.*', 'app/assets/**/*.*', 'app/config.json']
+      if (config.isDevelopment) {
+        console.log('You can manage your prototype at:')
+        console.log(`http://localhost:${port}/manage-prototype`)
+        console.log('')
+      }
+      console.log('The Prototype Kit is now running at:')
+      console.log(`http://localhost:${port}`)
+      console.log('')
+
+      if (config.isProduction || !config.useBrowserSync) {
+        server.listen(port)
+      } else {
+        server.listen(proxyPort, () => {
+          syncChanges.sync({
+            port,
+            proxyPort,
+            files: ['.tmp/public/**/*.*', 'app/views/**/*.*', 'app/assets/**/*.*', 'app/config.json']
+          })
         })
-      })
+      }
+    }
+  } catch (e) {
+    if (config.isDevelopment) {
+      verboseLog('************************ STARTING ERROR SERVER ***************************')
+      runErrorServer(e)
+    } else {
+      throw e
     }
   }
-} catch (e) {
-  if (config.isDevelopment) {
-    verboseLog('************************ STARTING ERROR SERVER ***************************')
-    runErrorServer(e)
-  } else {
-    throw e
-  }
-}
+})()
