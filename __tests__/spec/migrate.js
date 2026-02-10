7beb04ba1c38e0ffd3de6913be0fd0f7dcdbb5fb
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-02-10 18:11:22 +0000

    Move explicit import of `govukServiceNavigation` out of `main.html`
    The macro is not explicitely called, so it could be confusing for users
    to see it in the `layouts/main.html` file of their prototype.
    
    While there's no UI for it, this is something that can be achieved
    by accessing http://localhost:3000/manage-prototype/plugins/uninstall?package=govuk-frontend.

--- __tests__/spec/migrate.js
+++ __tests__/spec/migrate.js
@@ -173,7 +173,6 @@ https://prototype-kit.service.gov.uk/docs/how-to-use-layouts
 #}
 
 {% extends "govuk-prototype-kit/layouts/govuk-branded.njk" %}
-{% from "govuk/components/service-navigation/macro.njk" import govukServiceNavigation %}
 `
     )
   })
