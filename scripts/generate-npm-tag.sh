638c001508ed57f2cd8a5ea802cf06379ef84ab6
Nick Colley <2445413+NickColley@users.noreply.github.com>
2026-09-21 17:33:23 +0100

    Port publish-to-npm from govuk-frontend
    Changes:
    - remove references to npm workspaces which this project does not use.
    - convert the tests to commonjs
    - update the generate-npm-tag.sh to not require a get-versions script
    - call the script directly to avoid requiring a hook in the package.json

---
+++ scripts/generate-npm-tag.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+PACKAGE_NAME=$(npm pkg get name)
+
+# Use first argument as version or get it from the package.json
+PACKAGE_VERSION=${1:-$(npm pkg get version)}
+
+# Use second argument as latest published version or get latest tag published on Github
+LATEST_PUBLISHED_VERSION=${2:-$(npm view "$PACKAGE_NAME" version)}
+
+version() { echo "$@" | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }'; }
+
+if echo "$PACKAGE_VERSION" | grep -q "internal"; then
+  NPM_TAG="internal"
+elif echo "$PACKAGE_VERSION" | grep -q "-"; then
+  NPM_TAG="next"
+elif [ $(version "$PACKAGE_VERSION") -ge $(version "$LATEST_PUBLISHED_VERSION") ]; then
+  NPM_TAG="latest"
+else
+  major_version_num=$(echo "$PACKAGE_VERSION" | cut -d. -f1)
+  NPM_TAG="latest-v$major_version_num"
+fi
+
+echo "$NPM_TAG"
