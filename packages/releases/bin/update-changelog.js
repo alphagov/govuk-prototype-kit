f11dc485f6b6d96a164c1b6d90285a48392329d1
Romaric Pascal <romaric.pascal@dsit.gov.uk>
2026-09-25 17:47:45 +0100

    Make `update-changelog` a command of `@govuk-prototype-kit/release`
    Not much gain besides being regrouped with the rest of the release.
    
    The script can now receive the path to the CHANGELOG file to accomodate the CHANGELOG not being in the folder of the package whose version is being updated.

--- packages/releases/bin/update-changelog.js
+++ packages/releases/bin/update-changelog.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 const { resolve } = require('path')
 
-const { updateChangelog } = require('@govuk-prototype-kit/release/changelog-release-helper.js')
+const { updateChangelog } = require('../changelog-release-helper.js')
 
 // npm exposes these environment variable as part of the lifecycle hooks
 // (https://github.com/npm/cli/blob/c97b39b1e3436cd20a67ab5f4012a5f395c538b9/workspaces/libnpmversion/lib/version.js#L100-L103)
@@ -15,4 +17,7 @@ console.log(
   `Updating changelog from version ${previousVersion} to ${newVersion}...`
 )
 
-updateChangelog(resolve(__dirname, '../../CHANGELOG.md'), newVersion, previousVersion)
+// Use `resolve` to find the CHANGELOG.md file relative to the current working directory
+const changelogPath = resolve(process.argv[2] ?? 'CHANGELOG.md')
+
+updateChangelog(changelogPath, newVersion, previousVersion)
